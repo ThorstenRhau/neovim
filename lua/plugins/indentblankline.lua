b815a0c2c52f82dd504dc3838a446a4b4e9fae7f
Thorsten Rhau <thorsten@rhau.se>
2023-12-10 15:25:47 +0100

    Added line indentation plugins

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -1,42 +1,21 @@
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        event = "InsertEnter",
+        event = { "BufReadPre", "BufNewFile" },
         opts = {
             indent = {
-                char = "┆",
-                smart_indent_cap = true,
-                priority = 2,
-            },
-            -- whitespace = { highlight = { "Whitespace", "NonText" } },
-            whitespace = {
-                highlight = {
-                    "Function",
-                    "Label",
-                },
-                remove_blankline_trail = true,
-            },
-            scope = {
-                enabled = true,
-                show_start = true,
-                show_end = true,
                 char = "│",
-                exclude = { language = {} },
-                highlight = {
-                    "Function",
-                    "Label",
-                },
+                tab_char = "│",
             },
+            scope = { enabled = false },
             exclude = {
                 filetypes = {
                     "help",
-                    "Trouble",
                     "lazy",
                     "mason",
-                    "man",
                 },
             },
         },
+        main = "ibl",
     },
 }
