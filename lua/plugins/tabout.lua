66c80276e1ebaeb653af2529e7dcd4c743039480
Thorsten Rhau <thorsten@rhau.se>
2023-12-10 14:25:37 +0100

    Added 'tabout' plugin

---
+++ lua/plugins/tabout.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "abecodes/tabout.nvim",
+        lazy = true,
+        event = "InsertEnter",
+        dependencies = {
+            "nvim-treesitter",
+        },
+        opts = {},
+    },
+}
