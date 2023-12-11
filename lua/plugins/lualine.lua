07bcfb27fd117c175bf24877ce924b243caf3a9a
Thorsten Rhau <thorsten@rhau.se>
2023-12-11 19:53:42 +0100

    Reworked lualine configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,30 +7,34 @@ return {
         },
         opts = {
             options = {
-                icons_enabled = true,
-                theme = "auto",
-                component_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {},
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                globalstatus = false,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                },
+                component_separators = "|",
+                section_separators = { left = "", right = "" },
             },
-            extensions = {
-                "quickfix",
-                "fugitive",
-                "fzf",
-                "lazy",
-                "symbols-outline",
-                "trouble",
+            sections = {
+                lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+                lualine_b = { "filename", "branch", { "diff", colored = false } },
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = { "filetype", "progress" },
+                lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+            },
+            inactive_sections = {
+                lualine_a = { "filename" },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {
+                lualine_a = {
+                    {
+                        "buffers",
+                        separator = { left = "", right = "" },
+                        right_padding = 2,
+                        symbols = { alternate_file = "" },
+                    },
+                },
             },
         },
     },
