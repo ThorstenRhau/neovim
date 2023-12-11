f1b65f268751ec85675764fd1d023dd2179adfb9
Thorsten Rhau <thorsten@rhau.se>
2023-12-11 21:09:15 +0100

    Replaced black with ruff for formatting and diagnostics

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -4,10 +4,11 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.jq,
-		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.isort,
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.diagnostics.ruff,
+		null_ls.builtins.formatting.ruff,
 	},
 })
