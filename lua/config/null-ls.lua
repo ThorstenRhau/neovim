3b9d2ebbbe0ddf04654261f43067cbe03f22b2ca
Thorsten Rhau <thorsten@rhau.se>
2023-12-13 18:43:24 +0100

    Markdown and Bash lsp, formatters and linters

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -7,7 +7,9 @@ null_ls.setup({
 		null_ls.builtins.formatting.isort,
 		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.diagnostics.markdownlint,
 		null_ls.builtins.diagnostics.eslint,
 	},
 })
