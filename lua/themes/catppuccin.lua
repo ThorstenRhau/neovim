56eb1ee50456790248085956e87fb911e4bb17f7
Thorsten Rhau <github@thorre.se>
2023-12-16 10:04:01 +0100

    Shading for inactive window in catppuccin

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -15,9 +15,9 @@ return {
             show_end_of_buffer = false,         -- shows the '~' characters after the end of buffers
             term_colors = false,                -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
-                enabled = false,                -- dims the background color of inactive window
+                enabled = true,                 -- dims the background color of inactive window
                 shade = "dark",
-                percentage = 0.20,              -- percentage of the shade to apply to the inactive window
+                percentage = 0.10,              -- percentage of the shade to apply to the inactive window
             },
             no_italic = false,                  -- Force no italic
             no_bold = false,                    -- Force no bold
