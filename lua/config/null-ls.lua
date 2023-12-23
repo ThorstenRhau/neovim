da1f0b2c9b83c065c434c398f61807df31c8be37
Thorsten Rhau <github@thorre.se>
2023-12-23 11:02:41 +0100

    Moved null-ls configuration to mason-null-ls

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -1,20 +1,20 @@
-local null_ls = require("null-ls")
-
-null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.jq,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.black,
-        -- null_ls.builtins.diagnostics.pylint,
-        null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.formatting.prettier.with({
-            -- Force wrap when specified in .editorconfig
-            extra_args = { "--prose-wrap", "always" },
-        }),
-        -- null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.taplo,
-    },
-})
+--local null_ls = require("null-ls")
+--
+--null_ls.setup({
+--    sources = {
+--        null_ls.builtins.formatting.stylua,
+--        null_ls.builtins.formatting.jq,
+--        null_ls.builtins.formatting.isort,
+--        null_ls.builtins.formatting.black,
+--        -- null_ls.builtins.diagnostics.pylint,
+--        null_ls.builtins.formatting.shfmt,
+--        null_ls.builtins.diagnostics.shellcheck,
+--        null_ls.builtins.formatting.prettier.with({
+--            -- Force wrap when specified in .editorconfig
+--            extra_args = { "--prose-wrap", "always" },
+--        }),
+--        -- null_ls.builtins.diagnostics.markdownlint,
+--        null_ls.builtins.diagnostics.eslint,
+--        null_ls.builtins.formatting.taplo,
+--    },
+--})
