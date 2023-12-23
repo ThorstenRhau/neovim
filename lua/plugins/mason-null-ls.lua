8c356a576f6ef28bd3c44056f73b0b4ab5c53a47
Thorsten Rhau <github@thorre.se>
2023-12-23 11:02:41 +0100

    Moved null-ls configuration to mason-null-ls

--- lua/plugins/mason-null-ls.lua
+++ lua/plugins/mason-null-ls.lua
@@ -15,7 +15,30 @@ return {
             },
         },
 
-        { "nvimtools/none-ls.nvim" },
+        {
+            "nvimtools/none-ls.nvim",
+            init = function()
+                local null_ls = require("null-ls")
+                null_ls.setup({
+                    sources = {
+                        null_ls.builtins.formatting.stylua,
+                        null_ls.builtins.formatting.jq,
+                        null_ls.builtins.formatting.isort,
+                        null_ls.builtins.formatting.black,
+                        -- null_ls.builtins.diagnostics.pylint,
+                        null_ls.builtins.formatting.shfmt,
+                        null_ls.builtins.diagnostics.shellcheck,
+                        null_ls.builtins.formatting.prettier.with({
+                            -- Force wrap when specified in .editorconfig
+                            extra_args = { "--prose-wrap", "always" },
+                        }),
+                        -- null_ls.builtins.diagnostics.markdownlint,
+                        null_ls.builtins.diagnostics.eslint,
+                        null_ls.builtins.formatting.taplo,
+                    },
+                })
+            end,
+        },
     },
     config = function()
         ---@diagnostic disable-next-line: missing-fields
