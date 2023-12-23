69459cf17c43e505535d2cbe68cc1897b40fb216
Thorsten Rhau <github@thorre.se>
2023-12-23 18:48:09 +0100

    Corrected shortcut mapping

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -20,7 +20,7 @@ return {
             end,
         })
 
-        vim.keymap.set("n", "<leader>l", function()
+        vim.keymap.set("n", "<leader>cl", function()
             lint.try_lint()
         end, { desc = "Trigger linting for current file" })
     end,
