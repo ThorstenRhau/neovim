a8e49452344440962d78e291f4f51bacc130969b
Thorsten Rhau <github@thorre.se>
2023-12-24 11:51:24 +0100

    Added gitcommit linter
    Configuration of filetype to lint and automatic installation in Mason

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -7,6 +7,7 @@ return {
         local lint = require("lint")
 
         lint.linters_by_ft = {
+            gitcommit = { "gitlint" },
             python = { "pylint" },
             sh = { "shellcheck" },
         }
