787dab270d2a73e1169c4fe69f50243a0b634578
Thorsten Rhau <github@thorre.se>
2023-12-24 17:14:20 +0100

    Added 'ToggleTerm' plugin
    Including key mappings for whichkey

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,24 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    lazy = true,
+    cmd = { "ToggleTerm" },
+
+    config = function()
+        require("toggleterm").setup({
+            -- Custom config goes here
+        })
+        ---@diagnostic disable-next-line: lowercase-global
+        function set_terminal_keymaps()
+            local opts = { buffer = 0 }
+            vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+            vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+            vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+            vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+            vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+            vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+            vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+        end
+        vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+    end,
+}
