30d10bcf8da054822fe749abbd39383a3cc3790e
Thorsten Rhau <github@thorre.se>
2023-12-24 18:03:56 +0100

    Loading 'telescope' plugins properly
    Using the 'require' statement to load and initialize the all functionality of
    the telescope extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,27 +8,31 @@ return {
         "BurntSushi/ripgrep",
         "nvim-telescope/telescope-fzf-native.nvim",
         "debugloop/telescope-undo.nvim",
-        "nvim-telescope/telescope-file-browser.nvim",
         "AckslD/nvim-neoclip.lua",
     },
-    opts = {
-        defaults = {
-            layout_strategy = "cursor",
-            layout_config = {
-                height = 0.95,
-                width = 0.95,
+    config = function()
+        require("telescope").setup({
+            defaults = {
+                layout_strategy = "cursor",
+                layout_config = {
+                    height = 0.95,
+                    width = 0.95,
+                },
             },
-        },
-        extensions = {
-            fzf = {
-                fuzzy = true, -- false will only do exact matching
-                override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                -- the default case_mode is "smart_case"
+            extensions = {
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
+                undo = {},
+                neoclip = {},
             },
-            undo = {},
-            file_browser = {},
-        },
-    },
+            require("telescope").load_extension("fzf"),
+            require("telescope").load_extension("undo"),
+            require("telescope").load_extension("neoclip"),
+        })
+    end,
 }
