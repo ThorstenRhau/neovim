32b99e81d51da00c02ea0e383a0725914a8fa039
Thorsten Rhau <github@thorre.se>
2023-12-25 11:30:15 +0100

    Added 'neogit' plugin
    This may become a replacement to 'Git'

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    lazy = true,
+    cmd = { "Neogit" },
+    config = true,
+    opts = {},
+}
