f7ec01e8aabf8d24ab0e71ecca8e76c5748a7103
Thorsten Rhau <github@thorre.se>
2023-12-26 18:00:23 +0100

    Using neovim default marks keymappings

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -2,7 +2,7 @@ return {
     "chentoast/marks.nvim",
     event = { "BufReadPost", "BufNewFile", "BufWritePre" },
     opts = {
-        default_mappings = true,
+        default_mappings = false,
         refresh_interval = 500,
         excluded_filetypes = {},
     },
