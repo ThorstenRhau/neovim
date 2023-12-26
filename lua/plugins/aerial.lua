34c1dfacfde724bf873471bf087b41f310f8e5cf
Thorsten Rhau <github@thorre.se>
2023-12-26 12:53:10 +0100

    Replaced 'SymbolsOutline' wiht 'Aerial'
    Code outline whichkey mappings ahve been updated
    
    The Aerial plugin looks better and loads faster

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,9 @@
+return {
+    "stevearc/aerial.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    cmd = "AerialToggle",
+    opts = {},
+}
