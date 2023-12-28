0aad3c2e431f43dc7c1d6d9cf077bdc8e0f9f26a
Thorsten Rhau <github@thorre.se>
2023-12-28 20:07:43 +0100

    Improved luasnip loading
    Merged luasnip and friendly-snippets in to one plugin file so that they
    load properly

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,9 @@
+return {
+    "L3MON4D3/LuaSnip",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+}
