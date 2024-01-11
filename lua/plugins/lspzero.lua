4cd719276b05987fb55993008b0418d8aefd1507
Thorsten Rhau <github@thorre.se>
2024-01-11 02:47:27 +0100

    Added 'schemastore' to YAML and JSON LSPs

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -110,6 +110,7 @@ return {
                 { "hrsh7th/cmp-buffer" },
                 { "hrsh7th/cmp-path" },
                 { "saadparwaiz1/cmp_luasnip" },
+                { "b0o/schemastore.nvim" },
             },
             config = function()
                 -- Setting rounded border on LSP windows
@@ -130,6 +131,32 @@ return {
                     ensure_installed = {}, -- This is done by lua/plugins/mason-tool-installer.lua
                     handlers = {
                         lsp_zero.default_setup,
+                        jsonls = function()
+                            require("lspconfig").jsonls.setup({
+                                settings = {
+                                    json = {
+                                        schemas = require("schemastore").json.schemas(),
+                                        validate = { enable = true },
+                                    },
+                                },
+                            })
+                        end,
+                        yamlls = function()
+                            require("lspconfig").yamlls.setup({
+                                settings = {
+                                    yaml = {
+                                        schemaStore = {
+                                            -- You must disable built-in schemaStore support if you want to use
+                                            -- this plugin and its advanced options like `ignore`.
+                                            enable = false,
+                                            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                                            url = "",
+                                        },
+                                        schemas = require("schemastore").yaml.schemas(),
+                                    },
+                                },
+                            })
+                        end,
                     },
                 })
             end,
