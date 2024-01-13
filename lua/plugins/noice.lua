6b9db257f3e4237639def7f593303baa8f4adf50
Thorsten Rhau <github@thorre.se>
2024-01-13 20:52:21 +0100

    Enabled lsp_doc_border in 'noice' plugin

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -9,12 +9,13 @@ return {
     lazy = true,
     event = "VeryLazy",
     opts = {
-        -- routes = {
-        --     {
-        --         view = "notify",
-        --         filter = { event = "msg_showmode" },
-        --     },
-        -- },
+        presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = true, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = true, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
         cmdline = {
             view = "cmdline_popup", -- cmdline_popup or cmdline
         },
