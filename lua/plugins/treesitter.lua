acda885fd431f908712085c322f9918ab41e351d
Thorsten Rhau <github@thorre.se>
2024-01-13 12:02:35 +0100

    Better keymaps for incremental_selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,10 +21,10 @@ return {
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = "gnn", -- set to `false` to disable one of the mappings
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
                 },
             },
             ---@diagnostic disable-next-line: unused-local
