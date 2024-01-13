389403ff9c952119c15f56e911500ad5568fb9f5
Thorsten Rhau <github@thorre.se>
2024-01-13 21:27:44 +0100

    Removed duplicate configuration and cleaned up comments

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,11 +10,11 @@ return {
     event = "VeryLazy",
     opts = {
         presets = {
-            bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
-            long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = true, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = true, -- add a border to hover docs and signature help
+            bottom_search = true,
+            command_palette = false,
+            long_message_to_split = true,
+            inc_rename = true,
+            lsp_doc_border = true,
         },
         cmdline = {
             view = "cmdline_popup", -- cmdline_popup or cmdline
@@ -28,7 +28,6 @@ return {
             },
         },
         lsp = {
-            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
@@ -39,14 +38,6 @@ return {
             },
             signature = {
                 enabled = true,
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = true, -- add a border to hover docs and signature help
             },
             notify = {
                 enabled = true,
