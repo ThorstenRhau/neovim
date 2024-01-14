60da16ae8ed1839d03dda5d79ebb543af48d2a81
Thorsten Rhau <github@thorre.se>
2024-01-14 11:07:13 +0100

    Starting to add my own snippets
    Updated luasnip configuration to read the files in my_snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,6 +4,6 @@ return {
     version = "v2.*",
     build = "make install_jsregexp",
     config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/my_snippets" })
     end,
 }
