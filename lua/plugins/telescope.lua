ef9590faa4b4fc97ae399f5234f80498716b13b0
Thorsten Rhau <github@thorre.se>
2024-01-14 16:30:52 +0100

    Added configuration and keymaps to telescope-undo

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,7 +23,27 @@ return {
                     case_mode = "smart_case", -- or "ignore_case" or "respect_case"
                     -- the default case_mode is "smart_case"
                 },
-                undo = {},
+                undo = {
+                    use_delta = true,
+                    use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+                    side_by_side = true,
+                    diff_context_lines = vim.o.scrolloff,
+                    entry_format = "state #$ID, $STAT, $TIME",
+                    time_format = "",
+                    saved_only = false,
+                    mappings = {
+                        i = {
+                            ["<cr>"] = require("telescope-undo.actions").yank_additions,
+                            ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                            ["<C-cr>"] = require("telescope-undo.actions").restore,
+                        },
+                        n = {
+                            ["y"] = require("telescope-undo.actions").yank_additions,
+                            ["Y"] = require("telescope-undo.actions").yank_deletions,
+                            ["u"] = require("telescope-undo.actions").restore,
+                        },
+                    },
+                },
                 neoclip = {},
             },
             require("telescope").load_extension("fzf"),
