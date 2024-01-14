b120ff18c3763b552d6471aaab3e62d4cd2d3f67
Thorsten Rhau <github@thorre.se>
2024-01-14 12:08:09 +0100

    Increased delay, priority and re-wrote 'disabled'

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -5,20 +5,32 @@ return {
     opts = {
         symbol = "│",
         options = { try_as_border = true },
+        draw = {
+            delay = 200, -- 200 ms delay before drawing line
+            priority = 2,
+        },
     },
     init = function()
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {
-                "help",
-                "alpha",
-                "neo-tree",
-                "trouble",
-                "lazy",
-                "mason",
-                "checkhealth",
-            },
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+            desc = "Disable indentscope for certain filetypes",
             callback = function()
-                vim.b.miniindentscope_disable = true
+                local ignore_filetypes = {
+                    "aerial",
+                    "help",
+                    "lazy",
+                    "leetcode.nvim",
+                    "man",
+                    "mason",
+                    "neo-tree",
+                    "neogitstatus",
+                    "notify",
+                    "toggleterm",
+                    "Trouble",
+                }
+                if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+                    local bufnr = vim.api.nvim_get_current_buf()
+                    vim.b[bufnr].miniindentscope_disable = true
+                end
             end,
         })
     end,
