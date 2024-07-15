37510f28008f9411dd69ee76b46f51da3eb3585c
Thorsten Rhau <github@thorre.se>
2024-07-15 14:27:37 +0200

    Switched back to web-devicons
    mini.icons are note supported for this plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,8 +4,7 @@ return {
     branch = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        -- "nvim-tree/nvim-web-devicons",
-        "echasnovski/mini.icons",
+        "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
     lazy = true,
