0c56a899c2a71f05332db857d705feed813a4853
Thorsten Rhau <github@thorre.se>
2024-07-15 14:21:02 +0200

    Enabled default mappings

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -2,7 +2,7 @@ return {
     "chentoast/marks.nvim",
     event = { "BufReadPost", "BufNewFile", "BufWritePre" },
     opts = {
-        default_mappings = false,
+        default_mappings = true,
         refresh_interval = 500,
         excluded_filetypes = {},
     },
