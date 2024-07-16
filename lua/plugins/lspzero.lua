b444da7fcf31918293a08536de83954153dfa130
Thorsten Rhau <github@thorre.se>
2024-07-16 21:14:10 +0200

    LSP diagnistic virtual text only in pop-up
    All the diagnistic indications are still being displayed right next to
    the line number on the far left of the windows. The full diagnostic text
    is only displayed when the cursor is on the specific line.
    
    For me this makes the code much more readable.

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -123,6 +123,13 @@ return {
                 { "b0o/schemastore.nvim" },
             },
             config = function()
+                -- Setting up virtual text configuration
+                vim.diagnostic.config({
+                    virtual_text = false,
+                })
+                -- Show line diagnostics automatically in hover window
+                vim.o.updatetime = 250
+                vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
                 -- Setting rounded border on LSP windows
                 require("lspconfig.ui.windows").default_options.border = "rounded"
 
