2698fc6211bb0d503b20a505e0baf5c30dd8b9d5
Thorsten Rhau <github@thorre.se>
2024-07-16 19:03:42 +0200

    Added 'Telescope file_browser'

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -112,6 +112,7 @@ wk.add({
     --                                   ╰──────╯
     { "<leader>f", group = "Find" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fB", "<cmd>Telescope file_browser<cr>", desc = "File Browser" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
