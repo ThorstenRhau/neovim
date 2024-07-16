8819afca3d5a17aff9b692b740360cd1ed537f21
Thorsten Rhau <github@thorre.se>
2024-07-16 07:05:11 +0200

    More 'Oil' configuration
    Changed delete behavious so that things that are deleted are moved to
    .Trash, just to be safe.
    
    Showing hidden files but not '..' and '.git'.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,11 +6,20 @@ return {
     cmd = "Oil",
     opts = {
         default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = false,
         columns = {
             "icon",
-            -- "permissions",
-            -- "size",
-            -- "mtime",
+        },
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                return name == ".." or name == ".git"
+            end,
+        },
+        win_options = {
+            wrap = true,
+            cursorcolumn = true,
         },
         watch_for_changes = true,
         use_default_keymaps = true,
