205c1458d311035cb2f9064c98cbee82c43293f4
Thorsten Rhau <github@thorre.se>
2024-07-17 11:17:56 +0200

    Added modes to format function

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -8,7 +8,7 @@ return {
             function()
                 require("conform").format({ async = true, lsp_fallback = true })
             end,
-            mode = "",
+            mode = { "n", "v" },
             desc = "Format buffer",
         },
     },
