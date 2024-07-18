dfce57b9b96f18f021c773af21acdecfb2f09dba
Thorsten Rhau <github@thorre.se>
2024-07-18 16:46:25 +0200

    Cleanup and added new Window section + lots of new mappings

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -35,24 +35,32 @@ end
 -- Setting up register for 'which key' with keymappings
 local wk = require("which-key")
 wk.add({
+    --                                 ╭───────────╮
+    --                                 │ Root menu │
+    --                                 ╰───────────╯
     { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Undo" },
     { "<leader>l", "<cmd>Lazy<cr>", desc = "Lazy - plugin manager" },
     { "<leader>m", "<cmd>Mason<cr>", desc = "Mason - package manager" },
     { "<leader>o", "<cmd>Oil --float<cr>", desc = "Oil - file manager" },
     { "<leader>p", "<cmd>Pounce<cr>", desc = "Pounce - motion" },
-    { "<leader>s", '<cmd> lua require("persistence").load() <cr>', desc = "Restore last session" },
+    { "<leader>s", '<cmd>lua require("persistence").load()<cr>', desc = "Restore last session" },
     { "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal" },
     --                                  ╭────────╮
     --                                  │ Buffer │
     --                                  ╰────────╯
     { "<leader>b", group = "Buffer" },
-    { "<leader>bb", "<cmd>bprev<CR>", desc = "Previous" },
-    { "<leader>be", "<cmd>Neotree buffers<CR>", desc = "Neotree buffers" },
+    { "<leader>ba", "<cmd>b#<CR>", desc = "Switch to alternate" },
+    { "<leader>be", "<cmd>Neotree buffers<CR>", desc = "Neotree" },
+    { "<leader>bf", "<cmd>Telescope buffers<cr>", desc = "Find" },
     { "<leader>bl", "<cmd>ls<CR>", desc = "List" },
+    { "<leader>bm", "<cmd>bm<CR>", desc = "Go to next modified" },
+    { "<leader>bn", "<cmd>bnext<CR>", desc = "Previous" },
+    { "<leader>bp", "<cmd>bprevious<CR>", desc = "Previous" },
     --                                   ╭──────╮
     --                                   │ Code │
     --                                   ╰──────╯
     { "<leader>c", group = "Code" },
+    { "<leader>cA", "<cmd>LspInfo<CR>", desc = "LSP list" },
     {
         "<leader>cD",
         function()
@@ -60,9 +68,15 @@ wk.add({
         end,
         desc = "Go to declaration",
     },
-    { "<leader>cA", "<cmd>LspInfo<CR>", desc = "LSP list" },
     { "<leader>cF", "<cmd>ConformInfo<CR>", desc = "Formatters list" },
     { "<leader>cL", ListActiveLinters, desc = "Linters list" },
+    {
+        "<leader>cS",
+        function()
+            vim.lsp.buf.signature_help()
+        end,
+        desc = "Signature help",
+    },
     { "<leader>cT", "<Cmd>CBllline<CR>", desc = "Titled Line" },
     {
         "<leader>ca",
@@ -94,13 +108,6 @@ wk.add({
         desc = "Rename",
     },
     {
-        "<leader>cS",
-        function()
-            vim.lsp.buf.signature_help()
-        end,
-        desc = "Signature help",
-    },
-    {
         "<leader>ct",
         function()
             vim.lsp.buf.type_definition()
@@ -112,13 +119,13 @@ wk.add({
     --                                   ╰──────╯
     { "<leader>f", group = "Find" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "File" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
     { "<leader>fm", "<cmd>Telescope man_pages<cr>", desc = "man pages" },
-    { "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Notify messages" },
+    { "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Notifications" },
     { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
-    { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy find in buffer" },
+    { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "FZF in buffer" },
     { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Yanks" },
     { "<leader>fz", "<cmd>Telescope spell_suggest<cr>", desc = "Spelling suggestions" },
     --                                    ╭─────╮
@@ -126,8 +133,8 @@ wk.add({
     --                                    ╰─────╯
     { "<leader>g", group = "Git" },
     { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Blame" },
-    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Shows previous commits" },
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Interactive Git" },
+    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Interactive" },
     { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
     --                                 ╭───────────╮
     --                                 │ Interface │
@@ -154,6 +161,14 @@ wk.add({
     { "<leader>us", "<cmd>noh<cr>", desc = "Hide search results" },
     { "<leader>uv", toggle_virtual_text, desc = "Virtual Text" },
     { "<leader>uw", "<cmd>set wrap!<cr>", desc = "Wrap lines" },
+    --                                  ╭─────────╮
+    --                                  │ Window  │
+    --                                  ╰─────────╯
+    { "<leader>w", group = "Window" },
+    { "<leader>wc", "<cmd>close<cr>", desc = "Close" },
+    { "<leader>wo", "<cmd>only<cr>", desc = "Close all but not current" },
+    { "<leader>ws", "<cmd>split<cr>", desc = "Split horizontal" },
+    { "<leader>wv", "<cmd>vsplit<cr>", desc = "Split vertical" },
     --                                  ╭─────────╮
     --                                  │ Trouble │
     --                                  ╰─────────╯
