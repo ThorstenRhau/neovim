5a7007da8b64ba26975ba260dfd524750d2eaa1b
Thorsten Rhau <thorsten@rhau.se>
2024-07-20 10:15:53 +0200

    Added plugin Barbecue (winbar plugin)

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,11 @@
+return {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+        "SmiteshP/nvim-navic",
+        "echasnovski/mini.icons",
+    },
+    cmd = "Barbecue",
+    opts = {},
+}
