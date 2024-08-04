ee7de91a6692ed7ed64d8f1c668d0cd22995ead2
Thorsten Rhau <thorsten@rhau.se>
2024-08-04 08:35:38 +0200

    Updated build function for markdown-preview
    The previous version stopped working on nvim 0.10.1

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,8 +2,13 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
+    build = function(plugin)
+        if vim.fn.executable("npx") then
+            vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+        else
+            vim.cmd([[Lazy load markdown-preview.nvim]])
+            vim.fn["mkdp#util#install"]()
+        end
     end,
     keys = {
         {
