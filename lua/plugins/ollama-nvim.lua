577d426af8e8c55e8bec0fc35384e729003db435
Thorsten Rhau <thorsten@rhau.se>
2024-11-09 22:40:23 +0100

    Added Ollama integration on ctrl-o

---
+++ lua/plugins/ollama-nvim.lua
@@ -0,0 +1,31 @@
+return {
+    "nomnivore/ollama.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    cmd = {
+        "Ollama",
+        "OllamaModel",
+        "OllamaServe",
+        "OllamaServeStop",
+    },
+    keys = {
+        {
+            "<C-o>",
+            ":<c-u>lua require('ollama').prompt()<cr>",
+            desc = "ollama prompt",
+            mode = { "n", "v" },
+        },
+    },
+    opts = {
+        model = "codellama:latest",
+        url = "http://127.0.0.1:11434",
+        serve = {
+            on_start = false,
+            command = "ollama",
+            args = { "serve" },
+            stop_command = "pkill",
+            stop_args = { "-SIGTERM", "ollama" },
+        },
+    },
+}
