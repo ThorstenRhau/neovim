e8a77d8a8b7273bae754529c59d19623986eb7c2
Thorsten Rhau <thorsten@rhau.se>
2024-11-18 23:32:11 +0100

    Refactor model version in gen.nvim plugin configuration
    - Updated the model version from "qwen2.5-coder:3b" to "qwen2.5-coder:7b"

--- lua/plugins/gen.nvim.lua
+++ lua/plugins/gen.nvim.lua
@@ -1,18 +1,18 @@
 return {
     "David-Kunz/gen.nvim",
     opts = {
-        model = "qwen2.5-coder:3b", -- The default model to use.
-        quit_map = "q", -- set keymap to close the response window
-        retry_map = "<c-r>", -- set keymap to re-send the current prompt
-        accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-        host = "127.0.0.1", -- The host running the Ollama service.
-        port = "11434", -- The port on which the Ollama service is listening.
-        display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
-        show_prompt = false, -- Shows the prompt submitted to Ollama.
-        show_model = true, -- Displays which model you are using at the beginning of your chat session.
-        no_auto_close = false, -- Never closes the window automatically.
-        file = false, -- Write the payload to a temporary file to keep the command short.
-        hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+        model = "qwen2.5-coder:7b",
+        quit_map = "q",
+        retry_map = "<c-r>",
+        accept_map = "<c-cr>",
+        host = "127.0.0.1",
+        port = "11434",
+        display_mode = "float",
+        show_prompt = false,
+        show_model = true,
+        no_auto_close = false,
+        file = false,
+        hidden = false,
     },
     keys = {
         {
