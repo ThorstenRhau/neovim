029fce526a3d786965f440230953078043b4ef76
Thorsten Rhau <thorsten@rhau.se>
2024-11-26 21:53:58 +0100

    Adjusted better-diagnostic-virtual-text
    Above is now true and diagnostics lines are wrapped after 25 characters

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -157,13 +157,13 @@ return {
                     lsp_zero.default_keymaps({ buffer = bufnr })
                     require("better-diagnostic-virtual-text.api").setup_buf(bufnr, {
                         ui = {
-                            wrap_line_after = true, -- wrap the line after this length to avoid the virtual text is too long
+                            wrap_line_after = 25, -- wrap the line after this length to avoid the virtual text is too long
                             left_kept_space = 3, --- the number of spaces kept on the left side of the virtual text, make sure it enough to custom for each line
                             right_kept_space = 3, --- the number of spaces kept on the right side of the virtual text, make sure it enough to custom for each line
                             arrow = "  ",
                             up_arrow = "  ",
                             down_arrow = "  ",
-                            above = false, -- the virtual text will be displayed above the line
+                            above = true, -- the virtual text will be displayed above the line
                         },
                         priority = 2003, -- the priority of virtual text
                         inline = true,
