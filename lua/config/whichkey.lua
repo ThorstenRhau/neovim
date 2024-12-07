b24608bb38702cfaed1c36d1fb81627a9ee2baeb
Thorsten Rhau <thorsten@rhau.se>
2024-12-07 12:41:46 +0100

    Mapped new blame command from snacks.nvim

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -160,7 +160,7 @@ wk.add({
     --                                    │ Git │
     --                                    ╰─────╯
     { "<leader>g", group = "Git" },
-    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Blame" },
+    { "<leader>gb", "<cmd>lua Snacks.git.blame_line()<cr>", desc = "Blame" },
     { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Interactive" },
     { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
