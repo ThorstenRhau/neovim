fa0d19daf2e0a2c5e90ded90433d77aca15fcb79
Thorsten Rhau <thorsten@rhau.se>
2024-12-08 14:55:35 +0100

    Removed unneeded comment

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -2,7 +2,6 @@ return {
     "RRethy/vim-illuminate",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-        -- delay = 250,
         under_cursor = false,
         min_count_to_highlight = 1,
         large_file_cutoff = 2000,
