a81245f765334b9e266e7a847cc57e3751156730
Thorsten Rhau <thorsten@rhau.se>
2024-12-08 23:05:17 +0100

    Removed static theme configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -30,7 +30,6 @@ return {
         opts = {
             options = {
                 icons_enabled = true,
-                theme = "catppuccin",
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
                 disabled_filetypes = {
