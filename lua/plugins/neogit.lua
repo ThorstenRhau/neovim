425806d2c6bd9cac137b3a4e528de85bbd241593
Thorsten Rhau <thorsten@rhau.se>
2024-12-09 08:52:42 +0100

    graph_style is kitty and new settings

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -20,11 +20,20 @@ return {
             interval = 1000,
             enabled = true,
         },
-        graph_style = "unicode",
+        graph_style = "kitty",
+        console_timeout = 2000,
+        auto_show_console = true,
+        auto_close_console = true,
+        notification_icon = "󰊢",
         highlight = {
             italic = true,
             bold = true,
             underline = true,
+        },
+        signs = {
+            section = { "▶", "▼" },
+            item = { "▷", "▽" },
+            hunk = { "", "" },
         },
         kind = "tab",
         status = {
