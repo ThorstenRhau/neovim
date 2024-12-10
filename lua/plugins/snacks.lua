ad82d775cf8e18c9011ba64e742a51684a4360fe
Thorsten Rhau <thorsten@rhau.se>
2024-12-10 19:52:25 +0100

    Rewrite of 'snacks' plugin
    Changed:
    - bigfile
    - dashboard
    - notify
    - indent
    - quickfile
    - scope
    - scroll
    - statuscolumn
    - words
    
    keymappings and init function also got some attention

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,23 +3,57 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        bigfile = { enabled = true },
-        git = { enabled = true },
-        gitbrowse = { enabled = true },
-        notify = { enabled = true },
-        notifier = {
-            enabled = true,
-            timeout = 3000,
+        bigfile = {
+            size = 1 * 1024 * 1024, -- 1 MB
+            notify = true,
         },
-        styles = {
-            notification = {
-                wo = { wrap = true }, -- Wrap notifications
+        dashboard = {
+            row = nil, -- dashboard position. nil for center
+            col = nil, -- dashboard position. nil for center
+            preset = {
+                header = [[neovim :: by Thorsten]],
+            },
+            formats = {
+                header = { "%s", align = "center" },
+                text = { "%s", align = "center" },
+            },
+            sections = {
+                {
+                    section = "header",
+                },
             },
         },
+        git = { enabled = true },
+        gitbrowse = { enabled = true },
+        indent = { enabled = true },
+        notifier = {
+            timeout = 3000,
+            style = "compact",
+        },
+        notify = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
         terminal = { enabled = true },
+        words = { enabled = true },
     },
 
     keys = {
+        {
+            "<leader>z",
+            function()
+                Snacks.zen()
+            end,
+            desc = "Toggle Zen Mode",
+        },
+        {
+            "<leader>Z",
+            function()
+                Snacks.zen.zoom()
+            end,
+            desc = "Toggle Zoom",
+        },
         {
             "<leader>.",
             function()
@@ -70,6 +104,27 @@ return {
             desc = "Git Blame Line",
         },
         {
+            "<leader>gf",
+            function()
+                Snacks.lazygit.log_file()
+            end,
+            desc = "Lazygit Current File History",
+        },
+        {
+            "<leader>gg",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "Lazygit",
+        },
+        {
+            "<leader>gl",
+            function()
+                Snacks.lazygit.log()
+            end,
+            desc = "Lazygit Log (cwd)",
+        },
+        {
             "<leader>un",
             function()
                 Snacks.notifier.hide()
@@ -81,7 +136,7 @@ return {
             function()
                 Snacks.terminal()
             end,
-            desc = "Terminal",
+            desc = "Toggle Terminal",
         },
         {
             "]]",
@@ -98,24 +153,6 @@ return {
             end,
             desc = "Prev Reference",
             mode = { "n", "t" },
-        },
-        {
-            "<leader>N",
-            desc = "Neovim News",
-            function()
-                Snacks.win({
-                    file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-                    width = 0.6,
-                    height = 0.6,
-                    wo = {
-                        spell = false,
-                        wrap = false,
-                        signcolumn = "yes",
-                        statuscolumn = " ",
-                        conceallevel = 3,
-                    },
-                })
-            end,
         },
     },
     init = function()
@@ -139,12 +176,14 @@ return {
                 Snacks.toggle.line_number():map("<leader>ul")
                 Snacks.toggle
                     .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-                    :map("<leader>uR")
+                    :map("<leader>uc")
                 Snacks.toggle.treesitter():map("<leader>uT")
                 Snacks.toggle
                     .option("background", { off = "light", on = "dark", name = "Dark Background" })
                     :map("<leader>ub")
                 Snacks.toggle.inlay_hints():map("<leader>uh")
+                Snacks.toggle.indent():map("<leader>ug")
+                Snacks.toggle.dim():map("<leader>uD")
             end,
         })
     end,
