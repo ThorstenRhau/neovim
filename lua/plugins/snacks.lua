8503913d55810d1b59a4f769397ed988b042500e
Thorsten Rhau <thorsten@rhau.se>
2024-12-11 09:57:35 +0100

    toggle on in snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -36,6 +36,7 @@ return {
         scroll = { enabled = true },
         statuscolumn = { enabled = true },
         terminal = { enabled = true },
+        toggle = { enabled = true },
         words = { enabled = true },
     },
 
