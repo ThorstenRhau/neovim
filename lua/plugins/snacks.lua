54ae9a8c291344aa0305a705f67db2a518b8ed1f
Thorsten Rhau <thorsten@rhau.se>
2024-12-13 21:55:21 +0100

    Disabled snacks words in insert mode

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -37,7 +37,7 @@ return {
         statuscolumn = { enabled = true },
         terminal = { enabled = true },
         toggle = { enabled = true },
-        words = { enabled = true },
+        words = { modes = { "n", "c" } }, -- No word highlights in insert mode
     },
 
     keys = {
