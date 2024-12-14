d0b839bb4c70e9817d8ff698ae0ce86265b85b9c
Thorsten Rhau <thorsten@rhau.se>
2024-12-14 20:12:09 +0100

    Changed keymap for toggle terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -109,8 +109,7 @@ return {
         { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
         { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
         { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-        { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
-        { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+        { "<leader>t",  function() Snacks.terminal() end, desc = "Toggle Terminal" },
         { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
         { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
         -- stylua: ignore end
