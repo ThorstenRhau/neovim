f6a6757c4a659645cd2ffa169c858edfab15d96e
Thorsten Rhau <thorsten@rhau.se>
2024-12-14 22:09:33 +0100

    Changed dependency to fzf-lua

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,7 +2,7 @@ return {
     "NeogitOrg/neogit",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
+        "ibhagwan/fzf-lua",
         {
             "sindrets/diffview.nvim",
             cmd = {
