11099e565faa91a99f7f0bf1dacc4173d433b5b4
Thorsten Rhau <thorsten@rhau.se>
2024-12-15 16:52:16 +0100

    Removed scratch from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -99,8 +99,6 @@ return {
     -- stylua: ignore start
         { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
         { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
-        { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-        { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
         { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
         { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
         { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
