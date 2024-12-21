af8b8f730830d0ed52efb4ffb536c83b433ac3ab
Thorsten Rhau <thorsten@rhau.se>
2024-12-21 13:13:06 +0100

    Added neogit back to the list of active plugins

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,15 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "ibhagwan/fzf-lua",
+    },
+    cmd = "Neogit",
+    config = true,
+    opts = {
+        disable_insert_on_commit = true,
+        graph_style = "unicode",
+        process_spinner = false,
+    },
+}
