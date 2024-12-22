b3513cf5876cd07634216f0cb3ad4a9a870a4ef1
Thorsten Rhau <thorsten@rhau.se>
2024-12-22 16:38:25 +0100

    Added advanced LSP progress to snacks.notifier

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,4 +1,48 @@
 ---@diagnostic disable: undefined-global
+---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+local progress = vim.defaulttable()
+vim.api.nvim_create_autocmd("LspProgress", {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        if not client or type(value) ~= "table" then
+            return
+        end
+        local p = progress[client.id]
+
+        for i = 1, #p + 1 do
+            if i == #p + 1 or p[i].token == ev.data.params.token then
+                p[i] = {
+                    token = ev.data.params.token,
+                    msg = ("[%3d%%] %s%s"):format(
+                        value.kind == "end" and 100 or value.percentage or 100,
+                        value.title or "",
+                        value.message and (" **%s**"):format(value.message) or ""
+                    ),
+                    done = value.kind == "end",
+                }
+                break
+            end
+        end
+
+        local msg = {} ---@type string[]
+        progress[client.id] = vim.tbl_filter(function(v)
+            return table.insert(msg, v.msg) or not v.done
+        end, p)
+
+        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+        ---@diagnostic disable-next-line: param-type-mismatch
+        vim.notify(table.concat(msg, "\n"), "info", {
+            id = "lsp_progress",
+            title = client.name,
+            opts = function(notif)
+                notif.icon = #progress[client.id] == 0 and " "
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
 return {
     "folke/snacks.nvim",
     priority = 1000,
@@ -35,12 +79,13 @@ return {
         indent = { enabled = true },
         input = { enabled = true },
         notifier = {
+            height = { min = 1, max = 0.5 },
+            padding = true,
+            sort = { "added" }, -- sort only by time
             style = "compact",
             timeout = 8000,
-            sort = { "added" }, -- sort only by time
-            width = { min = 12, max = 0.3 },
-            height = { min = 1, max = 0.3 },
             top_down = true,
+            width = { min = 12, max = 0.3 },
         },
         quickfile = { enabled = true },
         scope = { enabled = true },
@@ -97,21 +142,21 @@ return {
     },
 
     keys = {
-    -- stylua: ignore start
-        { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-        { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
-        { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-        { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-        { "<leader>gw", function() Snacks.gitbrowse() end, desc = "Git Browse Web" },
-        { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-        { "<leader>gl", function() Snacks.lazygit() end, desc = "Lazygit" },
-        { "<leader>gL", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
-        { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-        { "<leader>t",  function() Snacks.terminal() end, desc = "Toggle Terminal" },
-        { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-        { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+        -- stylua: ignore start
+        { "<leader>z",  function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
+        { "<leader>Z",  function() Snacks.zen.zoom() end,                desc = "Toggle Zoom" },
+        { "<leader>n",  function() Snacks.notifier.show_history() end,   desc = "Notification History" },
+        { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
+        { "<leader>cR", function() Snacks.rename.rename_file() end,      desc = "Rename File" },
+        { "<leader>gw", function() Snacks.gitbrowse() end,               desc = "Git Browse Web" },
+        { "<leader>gb", function() Snacks.git.blame_line() end,          desc = "Git Blame Line" },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
+        { "<leader>gl", function() Snacks.lazygit() end,                 desc = "Lazygit" },
+        { "<leader>gL", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
+        { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
+        { "<leader>t",  function() Snacks.terminal() end,                desc = "Toggle Terminal" },
+        { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",              mode = { "n", "t" } },
+        { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",              mode = { "n", "t" } },
         -- stylua: ignore end
     },
 
