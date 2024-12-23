3b693e2697ff8388e277b9539907f01b42c54a32
Thorsten Rhau <thorsten@rhau.se>
2024-12-23 18:53:19 +0100

    Major rewrite of autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,79 +1,97 @@
+-- Utility function to create augroups
 local function augroup(name)
     return vim.api.nvim_create_augroup("my_" .. name, { clear = true })
 end
 
+-- Function to close or quit Neovim
+local function close_or_quit()
+    local win_count = #vim.api.nvim_list_wins()
+
+    if win_count == 1 then
+        if #vim.api.nvim_list_bufs() > 1 then
+            vim.notify("Cannot close the last window without quitting Neovim.", vim.log.levels.WARN)
+        else
+            vim.api.nvim_command("quit")
+        end
+    else
+        pcall(vim.api.nvim_command, "close")
+    end
+end
+
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
+    group = augroup("insert_mode"),
     pattern = "*",
-    command = "set nopaste",
+    callback = function()
+        vim.o.paste = false
+    end,
 })
 
--- Format options ,no help with comments :-)
-vim.cmd([[ autocmd FileType * set formatoptions-=cro ]])
-
--- Setting textwidth to 80 for markdown
+-- Remove 'c', 'r', 'o' from formatoptions for all filetypes
+-- No help with comments
 vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("remove_formatoptions_cro"),
+    pattern = "*",
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    end,
+})
+
+-- Markdown settings: textwidth, colorcolumn, wrap, spell
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("markdown_settings"),
     pattern = "markdown",
     callback = function()
         vim.bo.textwidth = 80
         vim.wo.colorcolumn = "80"
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
     end,
 })
 
--- close some filetypes with <q>
+-- Close specific filetypes with 'q' key
 vim.api.nvim_create_autocmd("FileType", {
     group = augroup("close_with_q"),
     pattern = {
-        "git",
         "checkhealth",
+        "git",
         "help",
         "lspinfo",
         "man",
         "notify",
         "qf",
+        "snacks_dashboard",
         "spectre_panel",
         "startuptime",
-        "tsplayground",
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
-        vim.keymap.set("n", "q", function()
-            Close_or_quit()
-        end, { buffer = event.buf, silent = true })
+        vim.keymap.set("n", "q", close_or_quit, {
+            buffer = event.buf,
+            silent = true,
+            noremap = true,
+            desc = "Close window",
+        })
     end,
 })
 
-function Close_or_quit()
-    local win_count = #vim.api.nvim_list_wins()
-
-    -- If there's only one window, try quitting Neovim
-    if win_count == 1 then
-        if #vim.api.nvim_list_bufs() > 1 then
-            vim.notify("Cannot close the last window without quitting Neovim.", vim.log.levels.WARN)
-        else
-            vim.cmd.quit()
-        end
-    else
-        -- Safely close the current window
-        pcall(vim.cmd.close) -- Use pcall to prevent errors if `close` fails
-    end
-end
-
--- resize splits if window got resized
-vim.api.nvim_create_autocmd({ "VimResized" }, {
+-- Resize splits when the window is resized
+vim.api.nvim_create_autocmd("VimResized", {
     group = augroup("resize_splits"),
     callback = function()
         vim.cmd("tabdo wincmd =")
     end,
 })
 
--- Check if we need to reload the file when it changed
+-- Check if we need to reload the file when it changes
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
     group = augroup("checktime"),
-    command = "checktime",
+    callback = function()
+        vim.cmd.checktime()
+    end,
 })
 
--- Highlight on yank
+-- Highlight text on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = augroup("highlight_yank"),
     callback = function()
@@ -81,31 +99,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
--- Markdown
-vim.api.nvim_create_autocmd("FileType", {
-    group = augroup("markdown_wrap_spell"),
-    pattern = { "markdown" },
-    callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.spell = true
-    end,
-})
-
+-- Git commit message settings
 vim.api.nvim_create_autocmd("FileType", {
     group = augroup("commit_message_settings"),
-    pattern = { "gitcommit" },
+    pattern = "gitcommit",
     callback = function()
-        vim.bo.textwidth = 72 -- Set text width for wrapping
-        vim.wo.colorcolumn = "50,73" -- Visual guide for 50/72 rule
-        vim.schedule(function() -- Schedule to avoid race conditions
-            vim.wo.spell = true -- Enable spell checking
-            vim.wo.wrap = true -- Enable line wrapping
+        vim.bo.textwidth = 72
+        vim.wo.colorcolumn = "50,73"
+        vim.schedule(function()
+            vim.wo.spell = true
+            vim.wo.wrap = true
         end)
     end,
 })
 
--- If neovim is opened with a directory as argument open oil-filemanager
+-- Open Oil file manager if Neovim is opened with a directory argument
 vim.api.nvim_create_autocmd("VimEnter", {
+    group = augroup("open_oil_on_dir"),
     callback = function()
         -- Iterate through all arguments to find a directory
         for _, arg in ipairs(vim.v.argv) do
@@ -117,5 +127,5 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end,
 })
 
--- Expand 'cc' into 'CodeCompanion' in the command line
--- vim.cmd([[cab cc CodeCompanion]])
+-- Command-line abbreviation: Expand 'cc' into 'CodeCompanion'
+vim.cmd([[cabbrev ff FzfLua]])
