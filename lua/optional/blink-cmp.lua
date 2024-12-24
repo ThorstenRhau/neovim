53199400ddf54b306fa246f6f4a5c7a33345d4c6
Thorsten Rhau <thorsten@rhau.se>
2024-12-24 21:09:38 +0100

    Finally a working super-TAB in blink-cmp

--- lua/optional/blink-cmp.lua
+++ lua/optional/blink-cmp.lua
@@ -73,7 +73,34 @@ return {
             },
         },
 
-        keymap = { preset = "enter" },
+        -- My super-TAB configuration
+        keymap = {
+            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-e>"] = { "hide", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
+
+            ["<Tab>"] = {
+                function(cmp)
+                    return cmp.select_next()
+                end,
+                "snippet_forward",
+                "fallback",
+            },
+            ["<S-Tab>"] = {
+                function(cmp)
+                    return cmp.select_prev()
+                end,
+                "snippet_backward",
+                "fallback",
+            },
+
+            ["<Up>"] = { "select_prev", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+            ["<C-up>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-down>"] = { "scroll_documentation_down", "fallback" },
+        },
 
         signature = { enabled = true },
 
