8cddf05cfd363edbab03617c68d98af31a4a3bec
Thorsten Rhau <thorsten@rhau.se>
2024-12-24 22:46:34 +0100

    Trouble focus, preview and key-re-maps

--- lua/optional/trouble.lua
+++ lua/optional/trouble.lua
@@ -2,17 +2,30 @@ return {
     {
         "folke/trouble.nvim",
         opts = {
+            mode = "document_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
             auto_close = true, -- Close when last error is fixed
+            focus = true, -- Focus the window when opened
+            preview = {
+                type = "float",
+                relative = "editor",
+                border = "rounded",
+                title = "Preview",
+                title_pos = "center",
+                position = { 0.7, 1 },
+                size = { width = 0.3, height = 0.3 },
+                zindex = 200,
+            },
         },
+
         cmd = "Trouble",
         keys = {
             {
-                "<leader>xx",
+                "<leader>xX",
                 "<cmd>Trouble diagnostics toggle<cr>",
                 desc = "Diagnostics (Trouble)",
             },
             {
-                "<leader>xX",
+                "<leader>xx",
                 "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
                 desc = "Buffer Diagnostics (Trouble)",
             },
