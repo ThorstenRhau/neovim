5a9eb6bf67550aa69522c790b1af6d9af4b6cbd2
Thorsten Rhau <thorsten@rhau.se>
2024-12-24 21:32:44 +0100

    Updated keymaps for 'trouble'

--- lua/optional/trouble.lua
+++ lua/optional/trouble.lua
@@ -27,12 +27,12 @@ return {
                 desc = "LSP Definitions / references / ... (Trouble)",
             },
             {
-                "<leader>xL",
+                "<leader>xl",
                 "<cmd>Trouble loclist toggle<cr>",
                 desc = "Location List (Trouble)",
             },
             {
-                "<leader>xQ",
+                "<leader>xq",
                 "<cmd>Trouble qflist toggle<cr>",
                 desc = "Quickfix List (Trouble)",
             },
