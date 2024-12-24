c8518c3c9e4bc61157c8204d8fcab29739b81474
Thorsten Rhau <thorsten@rhau.se>
2024-12-24 17:36:43 +0100

    Increased the min_keyword_length to 5 for buffer

--- lua/optional/blink-cmp.lua
+++ lua/optional/blink-cmp.lua
@@ -91,7 +91,7 @@ return {
                     min_keyword_length = 2,
                 },
                 buffer = {
-                    min_keyword_length = 3,
+                    min_keyword_length = 5,
                     max_items = 5,
                 },
             },
