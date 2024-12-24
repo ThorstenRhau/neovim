94dfe448c32f1d0dbc6b06517cdb7396802026da
Thorsten Rhau <thorsten@rhau.se>
2024-12-24 16:48:37 +0100

    Major rewrite for blink-cmp
    90% of the things work as I want them to now.

--- lua/optional/blink-cmp.lua
+++ lua/optional/blink-cmp.lua
@@ -2,8 +2,10 @@ return {
     "saghen/blink.cmp",
     dependencies = {
         "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim",
     },
     version = "*",
+
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
@@ -15,33 +17,84 @@ return {
 
         completion = {
             accept = { auto_brackets = { enabled = true } },
-            menu = {
-                border = "single",
-                draw = {
-                    treesitter = { "lsp" },
-                },
-            },
+
             documentation = {
-                window = { border = "single" },
                 auto_show = true,
                 auto_show_delay_ms = 250,
+                treesitter_highlighting = true,
+                window = { border = "rounded" },
+            },
+
+            list = {
+                selection = function(ctx)
+                    return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+                end,
+            },
+
+            menu = {
+                border = "rounded",
+
+                cmdline_position = function()
+                    if vim.g.ui_cmdline_pos ~= nil then
+                        local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+                        return { pos[1] - 1, pos[2] }
+                    end
+                    local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+                    return { vim.o.lines - height, 0 }
+                end,
+
+                draw = {
+                    columns = {
+                        { "kind_icon", "label", gap = 1 },
+                        { "kind" },
+                    },
+                    components = {
+                        kind_icon = {
+                            text = function(item)
+                                local kind = require("lspkind").symbol_map[item.kind] or ""
+                                return kind .. " "
+                            end,
+                            highlight = "CmpItemKind",
+                        },
+                        label = {
+                            text = function(item)
+                                return item.label
+                            end,
+                            highlight = "CmpItemAbbr",
+                        },
+                        kind = {
+                            text = function(item)
+                                return item.kind
+                            end,
+                            highlight = "CmpItemKind",
+                        },
+                    },
+                },
             },
         },
 
-        keymap = {
-            preset = "enter",
-            ["<C-y>"] = { "select_and_accept" },
-        },
+        keymap = { preset = "enter" },
 
-        signature = {
-            enabled = true,
-            window = { border = "single" },
-        },
+        signature = { enabled = true },
 
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
             cmdline = {}, -- Disable sources for command-line mode
+            providers = {
+                lsp = {
+                    min_keyword_length = 2, -- Number of characters to trigger porvider
+                },
+                path = {
+                    min_keyword_length = 0,
+                },
+                snippets = {
+                    min_keyword_length = 2,
+                },
+                buffer = {
+                    min_keyword_length = 3,
+                    max_items = 5,
+                },
+            },
         },
     },
-    opts_extend = { "sources.default" },
 }
