5e92f1cc9df12fecd9ebd33e86e6fc9424191859
Thorsten Rhau <thorsten@rhau.se>
2024-12-25 09:23:29 +0100

    Added cmdline configuration from reddit
    Thank you 'u/Indijanka'

--- lua/optional/blink-cmp.lua
+++ lua/optional/blink-cmp.lua
@@ -110,7 +110,19 @@ return {
 
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
-            cmdline = {}, -- Disable sources for command-line mode
+            -- cmdline = {}, -- Disable sources for command-line mode
+            cmdline = function()
+                local type = vim.fn.getcmdtype()
+                -- Search forward and backward
+                if type == "/" or type == "?" then
+                    return { "buffer" }
+                end
+                -- Commands
+                if type == ":" then
+                    return { "cmdline" }
+                end
+                return {}
+            end,
             providers = {
                 lsp = {
                     min_keyword_length = 2, -- Number of characters to trigger porvider
