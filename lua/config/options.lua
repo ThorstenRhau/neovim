a723d466907376623c86e64cc29b976ed79c7fc9
Thorsten Rhau <thorsten@rhau.se>
2024-12-25 20:26:15 +0100

    Set winblend to 25 for slight transparency of popups

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,7 +46,7 @@ o.wildmenu = true
 o.wildmode = "longest:full,full"
 o.wildoptions = "pum"
 o.winbar = "%=%m\\ %f"
-o.winblend = 0
+o.winblend = 25
 o.wrap = false
 opt.backspace = { "start", "eol", "indent" }
 opt.path:append({ "**" })
