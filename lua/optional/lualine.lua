833e446e300165ba1a4c5ba15399b0a7e4b12cec
Thorsten Rhau <thorsten@rhau.se>
2024-12-31 13:31:23 +0100

    (lualine) set tokyonight theme

--- lua/optional/lualine.lua
+++ lua/optional/lualine.lua
@@ -29,6 +29,7 @@ return {
         },
         opts = {
             options = {
+                theme = "tokyonight",
                 icons_enabled = true,
                 component_separators = { left = "", right = "" },
                 -- section_separators = { left = "", right = "" },
