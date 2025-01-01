ef8bdd3d6e3451ccc6ec657cd68e5ef022450242
Thorsten Rhau <thorsten@rhau.se>
2025-01-01 20:12:07 +0100

    (kanagawa) removed unneeded override in opts

--- lua/themes/kanagawa.lua
+++ lua/themes/kanagawa.lua
@@ -35,9 +35,5 @@ return {
         transparent = false, -- do not set background color
         dimInactive = true, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-            palette = {},
-            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
     },
 }
