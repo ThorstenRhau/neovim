533641d041720774016ab155f50275d22470e9d6
Thorsten Rhau <thorsten@rhau.se>
2025-01-08 19:16:04 +0100

    Updated lazy loading for noice

--- lua/optional/noice.lua
+++ lua/optional/noice.lua
@@ -4,7 +4,7 @@ return {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
     },
-    event = "VeryLazy",
+    event = { "CmdlineEnter", "InsertEnter" },
     opts = {
         presets = {
             bottom_search = true,
