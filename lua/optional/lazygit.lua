9fa0ffccd640fc16a9e3f9384a0a9cc0492a9ed5
Thorsten Rhau <thorsten@rhau.se>
2025-01-10 20:11:38 +0100

    Fixed comment spelling

--- lua/optional/lazygit.lua
+++ lua/optional/lazygit.lua
@@ -7,20 +7,20 @@ return {
         "LazyGitFilter",
         "LazyGitFilterCurrentFile",
     },
-    -- optional for floating window border decoration
+    -- Optional for floating window border decoration
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- Setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
         { "<leader>gl", "<cmd>:LazyGit<cr>", desc = "LazyGit" },
     },
     config = function()
-        -- Keymap to open Lazygit in a new buffer
+        -- Keymap to open LazyGit in a new buffer
         vim.keymap.set("n", "<leader>gl", function()
             vim.cmd("enew") -- Open a new buffer
-            vim.fn.termopen("LazyGit") -- Launch Lazygit in the terminal
+            vim.fn.termopen("LazyGit") -- Launch LazyGit in the terminal
             vim.cmd("startinsert") -- Enter insert mode for interaction
         end, { desc = "LazyGit in New Buffer" })
     end,
