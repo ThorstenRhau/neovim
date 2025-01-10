b9176c3955f5f56f501b47f05c8375a44514c676
Thorsten Rhau <thorsten@rhau.se>
2025-01-10 20:14:03 +0100

    Removed forced buffer for LazyGit

--- lua/optional/lazygit.lua
+++ lua/optional/lazygit.lua
@@ -11,17 +11,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    -- Setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
     keys = {
         { "<leader>gl", "<cmd>:LazyGit<cr>", desc = "LazyGit" },
     },
-    config = function()
-        -- Keymap to open LazyGit in a new buffer
-        vim.keymap.set("n", "<leader>gl", function()
-            vim.cmd("enew") -- Open a new buffer
-            vim.fn.termopen("LazyGit") -- Launch LazyGit in the terminal
-            vim.cmd("startinsert") -- Enter insert mode for interaction
-        end, { desc = "LazyGit in New Buffer" })
-    end,
 }
