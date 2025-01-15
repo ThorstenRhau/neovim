8431831c0b3945a5672ca77c26a34b3a0d965b1d
Thorsten Rhau <thorsten@rhau.se>
2025-01-15 19:24:05 +0100

    Removed dependency to 'codecompanion'

--- lua/optional/render-markdown.lua
+++ lua/optional/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     cmd = "RenderMarkdown",
-    ft = { "markdown", "codecompanion" },
+    ft = "markdown",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
     opts = {
         enabled = true,
