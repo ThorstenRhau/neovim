8e017acc885c7a86d842f5633fba0f45c6c78d5a
Thorsten Rhau <thorsten@rhau.se>
2025-01-17 14:36:49 +0100

    Removed dependency to fzf-lua from Neogit

--- lua/optional/neogit.lua
+++ lua/optional/neogit.lua
@@ -3,7 +3,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "sindrets/diffview.nvim",
-        "ibhagwan/fzf-lua",
+        -- "ibhagwan/fzf-lua",
     },
     cmd = "Neogit",
     config = true,
