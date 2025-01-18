e9f56e2298f49a403d8520ed34a6b53b40831d4a
Thorsten Rhau <thorsten@rhau.se>
2025-01-18 02:26:19 +0100

    added mini-nvim configuration

---
+++ lua/optional/mini-nvim.lua
@@ -0,0 +1,22 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    "echasnovski/mini.nvim",
+    dependencies = "echasnovski/mini.icons",
+    version = false,
+    event = "BufReadPost",
+    config = function()
+        require("mini.ai").setup()
+        require("mini.align").setup()
+        require("mini.bracketed").setup()
+        require("mini.comment").setup()
+        require("mini.operators").setup()
+        require("mini.pairs").setup()
+        require("mini.statusline").setup()
+        require("mini.surround").setup()
+    end,
+    keys = {
+        { "ga", desc = "Align text", mode = { "v" } },
+        { "gA", desc = "Align text interactive", mode = { "v" } },
+    },
+}
