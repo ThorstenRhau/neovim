d7e0c3ba338e3965b61c0f09414485f7c675eafc
Thorsten Rhau <thorsten@rhau.se>
2025-01-18 03:15:50 +0100

    removed marks.nvim plugin

--- lua/optional/marks.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "chentoast/marks.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        default_mappings = true,
-        refresh_interval = 500,
-        excluded_filetypes = {},
-    },
-}
