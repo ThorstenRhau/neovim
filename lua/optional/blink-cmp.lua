3c532e6e0ec0331b6c3d18d278e515296cf6b56a
Thorsten Rhau <thorsten@rhau.se>
2025-02-16 11:56:08 +0100

    (blink-cmp) disabled cmdline function

--- lua/optional/blink-cmp.lua
+++ lua/optional/blink-cmp.lua
@@ -75,18 +75,18 @@ return {
 
         sources = {
             default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-            cmdline = function()
-                local type = vim.fn.getcmdtype()
-                -- Search forward and backward
-                if type == "/" or type == "?" then
-                    return { "buffer" }
-                end
-                -- Commands
-                if type == ":" then
-                    return { "cmdline" }
-                end
-                return {}
-            end,
+            -- cmdline = function()
+            --     local type = vim.fn.getcmdtype()
+            --     -- Search forward and backward
+            --     if type == "/" or type == "?" then
+            --         return { "buffer" }
+            --     end
+            --     -- Commands
+            --     if type == ":" then
+            --         return { "cmdline" }
+            --     end
+            --     return {}
+            -- end,
             providers = {
                 lazydev = {
                     name = "LazyDev",
