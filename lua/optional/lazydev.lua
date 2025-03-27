569ffc3c2ccb5f9845e200a5f558f4e44eb35447
Thorsten Rhau <thorsten@rhau.se>
2025-03-27 20:43:00 +0100

    (LazyDev) fixed dependencies and path

--- lua/optional/lazydev.lua
+++ lua/optional/lazydev.lua
@@ -4,28 +4,27 @@ return {
     {
         "folke/lazydev.nvim",
         ft = "lua",
+        dependencies = {
+            "Bilal2453/luvit-meta",
+        },
         opts = {
             library = {
-                "$HOME/.config/nvim",
-                "$HOME/.config/wezterm",
+                vim.fn.expand("~/.config/nvim"),
                 { path = "luvit-meta/library", words = { "vim%.uv" } },
                 { path = "LazyVim", words = { "LazyVim" } },
-                { path = "wezterm-types", mods = { "wezterm" } },
             },
         },
     },
     { "Bilal2453/luvit-meta", lazy = true },
-    { -- optional blink completion source for require statements and module annotations
+    {
         "saghen/blink.cmp",
         opts = {
             sources = {
-                -- add lazydev to your completion providers
                 default = { "lazydev", "lsp", "path", "snippets", "buffer" },
                 providers = {
                     lazydev = {
                         name = "LazyDev",
                         module = "lazydev.integrations.blink",
-                        -- make lazydev completions top priority (see `:h blink.cmp`)
                         score_offset = 100,
                     },
                 },
