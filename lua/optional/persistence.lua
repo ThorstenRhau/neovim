e972929becaf3cbb2d749b9f9449a98c735f6049
Thorsten Rhau <thorsten@rhau.se>
2025-03-27 22:52:03 +0100

    (persistence) added missing configuration

--- lua/optional/persistence.lua
+++ lua/optional/persistence.lua
@@ -1,5 +1,31 @@
 return {
     "folke/persistence.nvim",
     event = "BufReadPre",
-    opts = {},
+    opts = {
+        dir = vim.fn.stdpath("state") .. "/sessions/",
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+    },
+    keys = {
+        {
+            "<leader>qs",
+            function()
+                require("persistence").load()
+            end,
+            desc = "Restore Session",
+        },
+        {
+            "<leader>ql",
+            function()
+                require("persistence").load({ last = true })
+            end,
+            desc = "Restore Last Session",
+        },
+        {
+            "<leader>qd",
+            function()
+                require("persistence").stop()
+            end,
+            desc = "Don't Save Current Session",
+        },
+    },
 }
