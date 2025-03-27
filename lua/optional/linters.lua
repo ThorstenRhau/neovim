7b25d7ba95cf91f6551849c96c0a80a72a0d068b
Thorsten Rhau <thorsten@rhau.se>
2025-03-27 22:40:11 +0100

    (linters) added local override

--- lua/optional/linters.lua
+++ lua/optional/linters.lua
@@ -1,25 +1,29 @@
-local vim = vim
 ---@module "lazy"
 ---@type LazySpec
 return {
     "mfussenegger/nvim-lint",
-    event = { "LspAttach" },
+    event = "LspAttach",
     config = function()
         local lint = require("lint")
 
         lint.linters_by_ft = {
             javascript = { "eslint_d" },
             json = { "jsonlint" },
-            lua = { "selene" }, -- Install 'selene' via homebrew instead of Mason
+            lua = { "selene" }, -- Installed via Homebrew
             python = { "ruff" },
             sh = { "shellcheck" },
             typescript = { "eslint_d" },
             yaml = { "yamllint" },
         }
 
+        local config = vim.fn.getcwd() .. "/.nvim-lint.lua"
+        if vim.fn.filereadable(config) == 1 then
+            dofile(config)
+        end
+
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
                 lint.try_lint()
