0a8f433dfc2544af085cd1fa9a740c94e7079ecb
Thorsten Rhau <thorsten@rhau.se>
2025-03-27 20:54:17 +0100

    (markview) using global icon setting

--- lua/optional/markview.lua
+++ lua/optional/markview.lua
@@ -3,10 +3,13 @@
 return {
     "OXY2DEV/markview.nvim",
     ft = "markdown",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    dependencies = {
+        { "nvim-treesitter/nvim-treesitter", ft = "markdown" },
+        { "echasnovski/mini.icons", lazy = true },
+    },
     opts = {
         preview = {
-            icon_provider = "mini",
+            icon_provider = nil, -- use global mini.icons setting
         },
     },
 }
