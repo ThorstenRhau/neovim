bb60c9571ac5e082e95db4d4ac5b47bf86db7cdb
Thorsten Rhau <thorsten@rhau.se>
2025-07-06 15:28:05 +0200

    (codecompanion) added plugin

---
+++ lua/optional/codecompanion.lua
@@ -0,0 +1,45 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    cmd = {
+        "CodeCompanion",
+        "CodeCompanionActions",
+        "CodeCompanionChat",
+        "CodeCompanionCmd",
+    },
+    keys = {
+        { "<leader>ci", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion Inline Assistant" },
+        { "<leader>cc", "<cmd>CodeCompanionChat<cr>", desc = "CodeCompanion Chat Buffer" },
+        { "<leader>cx", "<cmd>CodeCompanionCmd<cr>", desc = "CodeCompanion CMD Generator" },
+        { "<leader>cX", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions Palette" },
+    },
+    opts = {
+        adapters = {
+            openai = function()
+                return require("codecompanion.adapters").extend("openai", {
+                    env = { api_key = vim.env.OPENAI_API_KEY },
+                    parameters = {
+                        model = "gpt-4.1-mini",
+                        temperature = 0.2,
+                    },
+                })
+            end,
+        },
+        strategies = {
+            chat = {
+                adapter = "openai",
+            },
+            inline = {
+                adapter = "openai",
+            },
+            cmd = {
+                adapter = "openai",
+            },
+        },
+    },
+}
