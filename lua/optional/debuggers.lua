b9ba30ae2c09c3189b59968c89bccfc192669e1b
Thorsten Rhau <thorsten@rhau.se>
2025-12-02 15:08:39 +0100

    (debuggers) removed superfluous comments

--- lua/optional/debuggers.lua
+++ lua/optional/debuggers.lua
@@ -132,19 +132,9 @@ return {
     local dapui = require('dapui')
 
     require('mason-nvim-dap').setup({
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the languages you want
-      },
+      ensure_installed = {},
     })
 
     dapui.setup()
