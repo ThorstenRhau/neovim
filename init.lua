b458057fab9317ad39724cbeb786e2e21c801827
Thorsten Rhau <thorsten@rhau.se>
2025-12-03 16:46:05 +0100

    (init) added NEOVIM_BACKGROUND support function

--- init.lua
+++ init.lua
@@ -26,6 +26,12 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = "'"
 vim.opt.termguicolors = true
 
+-- Check for 'NEOVIM_BACKGROUND' environment variable to prevent startup flash.
+-- This should be set in your shell configuration (e.g., config.fish) based on system appearance.
+if os.getenv('NEOVIM_BACKGROUND') == 'light' then
+  vim.opt.background = 'light'
+end
+
 -- Lua module load (experimental)
 if vim.loader then
   vim.loader.enable()
