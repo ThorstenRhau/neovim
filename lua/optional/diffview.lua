be70c2cb83c4d3a1e0690d33f82c07578bc03f0f
Thorsten Rhau <thorsten@rhau.se>
2025-12-06 10:44:44 +0100

    feat(diffview): optimize configuration
    - Added keymaps:
      - <leader>gd: DiffviewOpen
      - <leader>gH: DiffviewFileHistory %
      - <leader>gq: DiffviewClose
    - Enabled enhanced diff highlighting.
    - Configured view options (winbar_info).
    - Added hook to disable line wrapping in diff views.

--- lua/optional/diffview.lua
+++ lua/optional/diffview.lua
@@ -1,12 +1,27 @@
+---@module "lazy"
+---@type LazySpec
 return {
   'sindrets/diffview.nvim',
-  opts = {},
-  cmd = {
-    'DiffviewFileHistory',
-    'DiffviewOpen',
-    'DiffviewClose',
-    'DiffviewToggleFiles',
-    'DiffviewFocusFiles',
-    'DiffviewRefresh',
+  cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
+  keys = {
+    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'DiffView Open' },
+    { '<leader>gH', '<cmd>DiffviewFileHistory %<cr>', desc = 'DiffView File History' },
+    { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = 'DiffView Close' },
+  },
+  opts = {
+    enhanced_diff_hl = true,
+    view = {
+      default = {
+        winbar_info = true,
+      },
+      file_history = {
+        winbar_info = true,
+      },
+    },
+    hooks = {
+      diff_buf_read = function()
+        vim.opt_local.wrap = false
+      end,
+    },
   },
 }
