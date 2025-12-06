f101232c13378e458dc3f1991846b9cc51c2745f
Thorsten Rhau <thorsten@rhau.se>
2025-12-06 09:13:24 +0100

    feat(blink): optimize blink.cmp configuration
    - Remove redundant default settings.
    - Add 'opts_extend' for better source extensibility.
    - Configure 'fuzzy' matcher with 'prefer_rust_with_warning' for stability.
    - Enhance buffer source to include all normal buffers.

--- lua/optional/blink-cmp.lua
+++ lua/optional/blink-cmp.lua
@@ -5,17 +5,13 @@ return {
   dependencies = { 'rafamadriz/friendly-snippets' },
   version = '1.*',
   event = { 'InsertEnter', 'CmdlineEnter' },
+  opts_extend = { 'sources.default' },
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     appearance = {
-      use_nvim_cmp_as_default = false,
       nerd_font_variant = 'normal',
-    },
-    cmdline = {
-      keymap = { preset = 'inherit' },
-      completion = { menu = { auto_show = true } },
     },
     completion = {
       accept = { auto_brackets = { enabled = true } },
@@ -27,7 +23,6 @@ return {
       },
       list = {
         selection = {
-          preselect = true,
           auto_insert = false,
         },
       },
@@ -46,6 +41,8 @@ return {
     keymap = {
       preset = 'super-tab',
     },
+
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
 
     signature = {
       enabled = true,
@@ -74,6 +71,13 @@ return {
         buffer = {
           min_keyword_length = 4,
           max_items = 5,
+          opts = {
+            get_bufnrs = function()
+              return vim.tbl_filter(function(bufnr)
+                return vim.bo[bufnr].buftype == ''
+              end, vim.api.nvim_list_bufs())
+            end,
+          },
         },
       },
     },
