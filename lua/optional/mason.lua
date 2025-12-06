a8e06f38d6e6a88a114d5107ef3467e3260bc8dd
Thorsten Rhau <thorsten@rhau.se>
2025-12-06 11:02:48 +0100

    fix(mason): clean up config and boost concurrency
    - Removed invalid 'cmd' option from 'opts' table.
    - Increased 'max_concurrent_installers' to 10 for faster bulk installations.

--- lua/optional/mason.lua
+++ lua/optional/mason.lua
@@ -2,7 +2,7 @@ return {
   'williamboman/mason.nvim',
   cmd = 'Mason',
   opts = {
-    cmd = 'Mason',
+    max_concurrent_installers = 10,
     ui = {
       icons = {
         package_installed = '✓',
