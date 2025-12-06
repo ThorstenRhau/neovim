0f1c7013615a2882da820cce78c81695c0fe2c71
Thorsten Rhau <thorsten@rhau.se>
2025-12-06 11:49:26 +0100

    feat(tabout): change act_as_shift_tab to true
    - Allows <S-Tab> to act as a normal shift-tab (unindent) if 'tabout' does not find a pair to jump out of.

--- lua/optional/tabout.lua
+++ lua/optional/tabout.lua
@@ -11,7 +11,7 @@ return {
       tabkey = '<Tab>',
       backwards_tabkey = '<S-Tab>',
       act_as_tab = true,
-      act_as_shift_tab = false,
+      act_as_shift_tab = true,
       default_tab = '<C-t>',
       default_shift_tab = '<C-d>',
       enable_backwards = true,
