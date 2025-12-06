ec70ffa1ab1bf2d363ec36b2bc382f2df3e91b64
Thorsten Rhau <thorsten@rhau.se>
2025-12-06 13:22:07 +0100

    fix(lsp): fixed a typo

--- lua/optional/lsp.lua
+++ lua/optional/lsp.lua
@@ -95,7 +95,7 @@ return {
 
         -- Enable inlay hints if the client supports them
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.supports_method('textDocument/inlayHint') then
+        if client and client:supports_method('textDocument/inlayHint') then
           vim.lsp.inlay_hint.enable(false, { bufnr = event.buf })
         end
       end,
