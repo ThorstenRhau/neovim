604e8947712e419065d92a33527e02357d073d7e
Thorsten Rhau <thorsten@rhau.se>
2025-12-09 16:07:14 +0100

    feat(lualine): Loading on BufReadPost and BufNewFile

--- lua/optional/lualine.lua
+++ lua/optional/lualine.lua
@@ -3,7 +3,7 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
-    event = 'VeryLazy',
+    event = { 'BufReadPost', 'BufNewFile' },
     dependencies = {
       { 'echasnovski/mini.icons' },
     },
