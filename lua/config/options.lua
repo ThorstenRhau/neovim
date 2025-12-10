a13553fafc353beb15440534835df66b87f6fe77
Thorsten Rhau <thorsten@rhau.se>
2025-12-10 20:17:57 +0100

    feat(options): Using vim.o instead of vim.opt where possible

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,49 +1,49 @@
 local opt = vim.opt
----@type table
+local o = vim.o
 local g = vim.g
 
-opt.autoindent = true
-opt.autoread = true
-opt.breakindent = true
-opt.clipboard = ''
-opt.cmdheight = 0
-opt.cmdwinheight = 10
-opt.completeopt = 'menu,menuone,noselect'
-opt.cursorline = true
-opt.foldenable = false
-opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-opt.foldlevel = 99
-opt.foldlevelstart = 99
-opt.foldmethod = 'expr'
-opt.grepformat = '%f:%l:%c:%m'
-opt.hlsearch = true
-opt.ignorecase = true
-opt.inccommand = 'split'
-opt.incsearch = true
-opt.jumpoptions = 'view'
-opt.laststatus = 3
-opt.linebreak = true
-opt.maxmempattern = 5000
-opt.mouse = 'nv'
-opt.number = false
-opt.pumblend = 0
-opt.scrolloff = 8
-opt.showcmd = true
-opt.sidescrolloff = 8
-opt.signcolumn = 'yes:1'
-opt.smartcase = true
-opt.smartindent = true
-opt.smarttab = true
-opt.smoothscroll = true
-opt.splitbelow = true
-opt.synmaxcol = 200
-opt.tabline = ''
-opt.updatetime = 250
-opt.wildmenu = true
-opt.wildmode = 'longest:full,full'
-opt.wildoptions = 'pum'
-opt.winblend = 0
-opt.wrap = false
+o.autoindent = true
+o.autoread = true
+o.breakindent = true
+o.clipboard = ''
+o.cmdheight = 0
+o.cmdwinheight = 10
+o.completeopt = 'menu,menuone,noselect'
+o.cursorline = true
+o.foldenable = false
+o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+o.foldlevel = 99
+o.foldlevelstart = 99
+o.foldmethod = 'expr'
+o.grepformat = '%f:%l:%c:%m'
+o.hlsearch = true
+o.ignorecase = true
+o.inccommand = 'split'
+o.incsearch = true
+o.jumpoptions = 'view'
+o.laststatus = 3
+o.linebreak = true
+o.maxmempattern = 5000
+o.mouse = 'nv'
+o.number = false
+o.pumblend = 0
+o.scrolloff = 8
+o.showcmd = true
+o.sidescrolloff = 8
+o.signcolumn = 'yes:1'
+o.smartcase = true
+o.smartindent = true
+o.smarttab = true
+o.smoothscroll = true
+o.splitbelow = true
+o.synmaxcol = 200
+o.tabline = ''
+o.updatetime = 250
+o.wildmenu = true
+o.wildmode = 'longest:full,full'
+o.wildoptions = 'pum'
+o.winblend = 0
+o.wrap = false
 opt.backspace = { 'indent', 'eol', 'start' }
 opt.path:append({ '**' })
 opt.spelllang = { 'en', 'sv' }
@@ -68,10 +68,10 @@ opt.wildignore:append({
 })
 
 -- Use spaces for tabs and whatnot
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.shiftround = true
-opt.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.shiftround = true
+o.expandtab = true
 
 -- Language providers
 g.loaded_node_provider = 0
@@ -81,22 +81,22 @@ g.loaded_python_provider = 0
 g.loaded_ruby_provider = 0
 
 -- Setting up undo
-opt.swapfile = false
-opt.backup = false
+o.swapfile = false
+o.backup = false
 local home = vim.env.HOME or vim.fn.expand('~')
 local undodir = home .. '/.local/share/nvim/undo'
 if vim.fn.isdirectory(undodir) == 0 then
   vim.fn.mkdir(undodir, 'p')
 end
-opt.undodir = undodir
-opt.undofile = true
-opt.undolevels = 10000
+o.undodir = undodir
+o.undofile = true
+o.undolevels = 10000
 
 -- Session management
 opt.sessionoptions:remove('blank') -- Not saving empty buffers
 
-opt.grepprg = vim.fn.executable('rg') == 1 and 'rg --vimgrep --smart-case --follow' or 'grep -n $* /dev/null'
+o.grepprg = vim.fn.executable('rg') == 1 and 'rg --vimgrep --smart-case --follow' or 'grep -n $* /dev/null'
 
 -- Partial title rewriting
-opt.titlestring = '%<%F - nvim'
-opt.title = true
+o.titlestring = '%<%F - nvim'
+o.title = true
