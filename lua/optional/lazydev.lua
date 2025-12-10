610ec4aac50f4695d9825d26bf9f78bcda7c8733
Thorsten Rhau <thorsten@rhau.se>
2025-12-10 20:06:49 +0100

    feat(lazydev): Configured integrations

--- lua/optional/lazydev.lua
+++ lua/optional/lazydev.lua
@@ -4,10 +4,17 @@ return {
   {
     'folke/lazydev.nvim',
     ft = 'lua',
+    ---@module 'lazydev'
+    ---@type lazydev.Config
     opts = {
       library = {
         { path = '~/.config/nvim/' },
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+      integrations = {
+        lspconfig = true,
+        cmp = false,
+        coq = false,
       },
     },
   },
