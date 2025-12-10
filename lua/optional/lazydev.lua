cfd47c4fdf1f8d509dd40cb22c0ff187defe27ea
Thorsten Rhau <thorsten@rhau.se>
2025-12-10 12:31:02 +0100

    feat(lazydev): Removed dependencies and changed path

--- lua/optional/lazydev.lua
+++ lua/optional/lazydev.lua
@@ -4,15 +4,11 @@ return {
   {
     'folke/lazydev.nvim',
     ft = 'lua',
-    dependencies = {
-      'Bilal2453/luvit-meta',
-    },
     opts = {
       library = {
-        vim.fn.expand('~/.config/nvim'),
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = '~/.config/nvim/' },
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
   },
-  { 'Bilal2453/luvit-meta', lazy = true },
 }
