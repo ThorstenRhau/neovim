fb3abccc4dbf28e9d0d644dd0563cc48a450096d
Thorsten Rhau <thorsten@rhau.se>
2025-12-13 18:10:55 +0100

    feat(gitsigns): Increased line_blame display delay

--- lua/optional/gitsigns.lua
+++ lua/optional/gitsigns.lua
@@ -29,7 +29,7 @@ return {
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = 'right_align',
-      delay = 1000,
+      delay = 1500,
     },
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
     preview_config = {
