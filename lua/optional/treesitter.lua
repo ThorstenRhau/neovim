f9c46d8a744f5e5ebd78a44c12de5710f002ec9f
Thorsten Rhau <thorsten@rhau.se>
2025-12-15 10:46:51 +0100

    feat(treesitter): Refactored configuration for 'main'

--- lua/optional/treesitter.lua
+++ lua/optional/treesitter.lua
@@ -1,113 +1,59 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-    keys = {
-      { '<c-space>', desc = 'Increment Selection' },
-      { '<bs>', desc = 'Decrement Selection', mode = 'x' },
-    },
-    opts = {
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local max = 256 * 1024
-          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-          return ok and stats and stats.size > max
-        end,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['a,'] = '@parameter.outer',
-            ['i,'] = '@parameter.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']f'] = '@function.outer',
-            [']c'] = '@class.outer',
-            ['],'] = '@parameter.inner',
-          },
-          goto_previous_start = {
-            ['[f'] = '@function.outer',
-            ['[c'] = '@class.outer',
-            ['[,'] = '@parameter.inner',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['>,'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<,'] = '@parameter.inner',
-          },
-        },
-      },
-      ensure_installed = {
-        'bash',
-        'comment',
-        'css',
-        'diff',
-        'fish',
-        'git_config',
-        'git_rebase',
-        'gitcommit',
-        'gitignore',
-        'html',
-        'javascript',
-        'json',
-        'lua',
-        'luadoc',
-        'make',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'query',
-        'regex',
-        'toml',
-        'typst',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
-      },
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate',
+  config = function()
+    local ts = require('nvim-treesitter')
 
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    event = { 'BufReadPost', 'BufNewFile' },
-  },
+    -- Install core parsers at startup
+    ts.install({
+      'bash',
+      'comment',
+      'css',
+      'diff',
+      'fish',
+      'git_config',
+      'git_rebase',
+      'gitcommit',
+      'gitignore',
+      'html',
+      'javascript',
+      'json',
+      'lua',
+      'luadoc',
+      'make',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'query',
+      'regex',
+      'toml',
+      'typst',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
+    })
+
+    -- Auto-install parsers on FileType
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(event)
+        local lang = vim.treesitter.language.get_lang(event.match) or event.match
+        local buf = event.buf
+
+        require('nvim-treesitter').install({ lang }, {
+          callback = function()
+            -- Enable highlighting after install completes
+            if vim.api.nvim_buf_is_valid(buf) then
+              vim.treesitter.start(buf, lang)
+            end
+          end,
+        })
+      end,
+    })
+  end,
 }
