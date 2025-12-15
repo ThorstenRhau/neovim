f8c06c06c1af2220b63928e8d6e4b8fe889af1df
Thorsten Rhau <thorsten@rhau.se>
2025-12-15 13:19:19 +0100

    feat(treesitter): Removed nvim-treesitter-textobjects dependency

--- lua/optional/treesitter.lua
+++ lua/optional/treesitter.lua
@@ -3,7 +3,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
     'nvim-treesitter/nvim-treesitter-context',
   },
   lazy = false,
