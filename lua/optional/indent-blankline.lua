dcd78701dcb55877190d63ffa9b3dbe79860ab4b
Thorsten Rhau <thorsten@rhau.se>
2025-12-22 21:01:54 +0100

    feat(indent-blankline): added new plugin

---
+++ lua/optional/indent-blankline.lua
@@ -0,0 +1,44 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  event = { 'BufReadPost', 'BufNewFile' },
+  keys = {
+    {
+      '<leader>ug',
+      function()
+        local ibl = require('ibl')
+        local config = require('ibl.config').get_config(0)
+        ibl.setup_buffer(0, { enabled = not config.enabled })
+      end,
+      desc = 'Toggle Indent Guides',
+    },
+  },
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {
+    scope = {
+      enabled = true,
+      show_start = true,
+      show_end = false,
+    },
+    indent = {
+      char = '│',
+    },
+    exclude = {
+      filetypes = {
+        'help',
+        'lazy',
+        'mason',
+        'neo-tree',
+        'oil',
+        'snacks_dashboard',
+        'snacks_notif',
+        'snacks_win',
+        'Trouble',
+        'trouble',
+      },
+    },
+  },
+}
