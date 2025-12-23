123165ccc0ea181ce526caf1b973b4329dcf199e
Thorsten Rhau <thorsten@rhau.se>
2025-12-23 11:39:17 +0100

    feat(vim-illuminate): new plugin

---
+++ lua/optional/vim-illuminate.lua
@@ -0,0 +1,31 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  'RRethy/vim-illuminate',
+  event = { 'BufReadPost', 'BufNewFile' },
+  config = function()
+    require('illuminate').configure({
+      providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+      },
+      delay = 250,
+      filetypes_denylist = {
+        'Trouble',
+        'checkhealth',
+        'help',
+        'lazy',
+        'mason',
+        'oil',
+        'trouble',
+      },
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { 'lsp' },
+      },
+      under_cursor = false,
+      modes_allowlist = { 'n' },
+    })
+  end,
+}
