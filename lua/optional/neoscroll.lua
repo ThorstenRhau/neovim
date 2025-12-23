cd183dd83849964754f62221545b88053ca25337
Thorsten Rhau <thorsten@rhau.se>
2025-12-23 19:17:22 +0100

    feat(neoscroll): more responsive scrolling config

--- lua/optional/neoscroll.lua
+++ lua/optional/neoscroll.lua
@@ -3,5 +3,12 @@
 return {
   'karb94/neoscroll.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    duration_multiplier = 0.6,
+    easing = 'sine',
+    hide_cursor = true,
+    stop_eof = true,
+    cursor_scrolls_alone = true,
+    respect_scrolloff = false,
+  },
 }
