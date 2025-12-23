adda6314238aea7465d0a6f6c27ad82ba316b854
Thorsten Rhau <thorsten@rhau.se>
2025-12-23 16:06:15 +0100

    feat(gitsigns): added missing keymaps and fixed api calls

--- lua/optional/gitsigns.lua
+++ lua/optional/gitsigns.lua
@@ -52,7 +52,8 @@ return {
         if vim.wo.diff then
           vim.cmd.normal({ ']c', bang = true })
         else
-          gs.next_hunk()
+          ---@diagnostic disable-next-line: param-type-mismatch
+          gs.nav_hunk('next')
         end
       end, 'Next Hunk')
 
@@ -60,7 +61,8 @@ return {
         if vim.wo.diff then
           vim.cmd.normal({ '[c', bang = true })
         else
-          gs.prev_hunk()
+          ---@diagnostic disable-next-line: param-type-mismatch
+          gs.nav_hunk('prev')
         end
       end, 'Prev Hunk')
 
@@ -74,7 +76,7 @@ return {
         gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
       end, 'Reset Hunk')
       map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
-      map('n', '<leader>ghu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+      map('n', '<leader>ghu', gs.reset_buffer_index, 'Unstage All Hunks')
       map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
       map('n', '<leader>ghp', gs.preview_hunk, 'Preview Hunk')
       map('n', '<leader>ghb', function()
@@ -83,9 +85,17 @@ return {
       map('n', '<leader>ghB', gs.toggle_current_line_blame, 'Toggle Line Blame')
       map('n', '<leader>ghd', gs.diffthis, 'Diff This')
       map('n', '<leader>ghD', function()
-        gs.diffthis('~')
-      end, 'Diff This ~')
+        ---@diagnostic disable-next-line: param-type-mismatch
+        gs.diffthis('~1')
+      end, 'Diff This ~1')
+      map('n', '<leader>ghP', gs.preview_hunk_inline, 'Preview Hunk Inline')
+      map('n', '<leader>ghq', gs.setqflist, 'Hunks to Quickfix')
+
+      -- Toggles
       map('n', '<leader>ghl', gs.toggle_linehl, 'Toggle Line Highlight')
+      map('n', '<leader>ghg', gs.toggle_signs, 'Toggle Signs')
+      map('n', '<leader>ghn', gs.toggle_numhl, 'Toggle Number Highlight')
+      map('n', '<leader>ghw', gs.toggle_word_diff, 'Toggle Word Diff')
 
       -- Text object
       map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
