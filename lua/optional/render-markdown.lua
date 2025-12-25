c5a0445dccc16bcad8f63e2c421f8746e5c2d8cf
Thorsten Rhau <thorsten@rhau.se>
2025-12-25 16:16:50 +0100

    feat(render-markdown): new plugin
    To view markdown in rendered mode in neovim press <leader>um

---
+++ lua/optional/render-markdown.lua
@@ -0,0 +1,21 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = 'markdown',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+  keys = {
+    {
+      '<leader>um',
+      function()
+        require('render-markdown').toggle()
+      end,
+      desc = 'Toggle Markdown Rendering',
+    },
+  },
+  opts = {
+    enabled = false,
+  },
+}
