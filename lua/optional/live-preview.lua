9d83f9a838ac5001f0f389cb06a405d8be8e414b
Thorsten Rhau <thorsten@rhau.se>
2025-12-30 16:03:47 +0100

    fix(live-preview): render-markdown keymap conflict

--- lua/optional/live-preview.lua
+++ lua/optional/live-preview.lua
@@ -12,8 +12,8 @@ return {
     picker = 'fzf-lua',
   },
   keys = {
-    { '<leader>ums', '<cmd>LivePreview start<cr>', desc = 'Start Live Preview' },
-    { '<leader>umc', '<cmd>LivePreview close<cr>', desc = 'Close Live Preview' },
-    { '<leader>ump', '<cmd>LivePreview pick<cr>', desc = 'Pick File to Preview' },
+    { '<leader>uMs', '<cmd>LivePreview start<cr>', desc = 'Start Live Preview' },
+    { '<leader>uMc', '<cmd>LivePreview close<cr>', desc = 'Close Live Preview' },
+    { '<leader>uMp', '<cmd>LivePreview pick<cr>', desc = 'Pick File to Preview' },
   },
 }
