da895b1fd6091fcaee2ad76c5f9867a54d6a5224
Thorsten Rhau <thorsten@rhau.se>
2025-12-30 17:00:41 +0100

    feat(neoscroll): only non-default values in config

--- lua/optional/neoscroll.lua
+++ lua/optional/neoscroll.lua
@@ -4,11 +4,6 @@ return {
   'karb94/neoscroll.nvim',
   event = 'VeryLazy',
   opts = {
-    duration_multiplier = 0.8,
     easing = 'sine',
-    hide_cursor = true,
-    stop_eof = true,
-    cursor_scrolls_alone = true,
-    respect_scrolloff = false,
   },
 }
