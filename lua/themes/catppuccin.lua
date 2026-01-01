b181f00e1d5a66e360be5e81505fac3a0365fdc9
Thorsten Rhau <thorsten@rhau.se>
2026-01-01 18:00:50 +0100

    feat(catppuccin): Melange color overrides for Latte and Mocha
    Based on https://github.com/savq/melange-nvim

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -29,6 +29,74 @@ return {
       no_italic = false,
       no_bold = false,
       no_underline = false,
+      color_overrides = {
+        mocha = {
+          -- Base colors
+          base = '#292522',
+          mantle = '#34302C',
+          crust = '#211E1C',
+          -- Surface colors
+          surface0 = '#403A36',
+          surface1 = '#5A524C',
+          surface2 = '#867462',
+          -- Overlay colors
+          overlay0 = '#9A8A7A',
+          overlay1 = '#AE9D8C',
+          overlay2 = '#C1A78E',
+          -- Text colors
+          subtext0 = '#D4C4B4',
+          subtext1 = '#E0D2C5',
+          text = '#ECE1D7',
+          -- Accent colors
+          rosewater = '#EBC9B4',
+          flamingo = '#B380B0',
+          pink = '#CF9BC2',
+          mauve = '#A3A9CE',
+          red = '#D47766',
+          maroon = '#BD8183',
+          peach = '#E49B5D',
+          yellow = '#EBC06D',
+          green = '#85B695',
+          teal = '#78997A',
+          sky = '#89B3B6',
+          sapphire = '#7B9695',
+          blue = '#7F91B2',
+          lavender = '#A3A9CE',
+        },
+        latte = {
+          -- Base colors
+          base = '#F1F1F1',
+          mantle = '#E9E1DB',
+          crust = '#D9D3CE',
+          -- Surface colors
+          surface0 = '#CEC7C1',
+          surface1 = '#C0B5AC',
+          surface2 = '#A98A78',
+          -- Overlay colors
+          overlay0 = '#9A7B68',
+          overlay1 = '#8B6C5A',
+          overlay2 = '#7D6658',
+          -- Text colors
+          subtext0 = '#6A5548',
+          subtext1 = '#5F4C40',
+          text = '#54433A',
+          -- Accent colors
+          rosewater = '#D4A088',
+          flamingo = '#BE79BB',
+          pink = '#904180',
+          mauve = '#465AA4',
+          red = '#BF0021',
+          maroon = '#C77B8B',
+          peach = '#BC5C00',
+          yellow = '#A06D00',
+          green = '#3A684A',
+          teal = '#6E9B72',
+          sky = '#3D6568',
+          sapphire = '#739797',
+          blue = '#7892BD',
+          lavender = '#465AA4',
+        },
+      },
       styles = {
         booleans = { 'bold' },
         comments = { 'italic' },
