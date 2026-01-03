739d08fa2d4d693018777ed1b45d4298c91d6df9
Thorsten Rhau <thorsten@rhau.se>
2026-01-03 17:36:20 +0100

    fix(vim-matchup): loading plugin earlier
    The VeryLazy event loaded the plugin to late. Loading on BufReadPost or
    BufNewFile now and the plugin works as intended.
    
    Reduced the show and hide delays for better responsiveness.

--- lua/optional/vim-matchup.lua
+++ lua/optional/vim-matchup.lua
@@ -2,11 +2,11 @@
 ---@type LazySpec
 return {
   'andymass/vim-matchup',
-  event = 'VeryLazy',
+  event = { 'BufReadPost', 'BufNewFile' },
   init = function()
     vim.g.matchup_matchparen_deferred = 1
-    vim.g.matchup_matchparen_deferred_show_delay = 300
-    vim.g.matchup_matchparen_deferred_hide_delay = 500
+    vim.g.matchup_matchparen_deferred_show_delay = 200
+    vim.g.matchup_matchparen_deferred_hide_delay = 100
     vim.g.matchup_matchparen_offscreen = { method = 'popup' }
   end,
   keys = {
