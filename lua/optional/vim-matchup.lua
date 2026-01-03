e879b516fc1128510a084f282746978cf57580e1
Thorsten Rhau <thorsten@rhau.se>
2026-01-03 18:51:21 +0100

    feat(terminal): ignoring the terminal with plugins
    The following plugins are now ignoring the terminal
    - gitsigns
    - indent-blankline
    - vim-illuminate
    - vim-matchup

--- lua/optional/vim-matchup.lua
+++ lua/optional/vim-matchup.lua
@@ -8,6 +8,12 @@ return {
     vim.g.matchup_matchparen_deferred_show_delay = 100
     vim.g.matchup_matchparen_deferred_hide_delay = 100
     vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+
+    vim.api.nvim_create_autocmd('TermOpen', {
+      callback = function()
+        vim.b.matchup_matchparen_enabled = 0
+      end,
+    })
   end,
   keys = {
     {
