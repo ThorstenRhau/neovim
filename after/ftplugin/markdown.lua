820bba1114048cbc53fb3de0488dceef8d27e044
Thorsten Rhau <thorsten@rhau.se>
2026-01-04 23:38:48 +0100

    feat(hoverdoc): disabled spelling
    Only enable spelling for regular files.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,11 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
-vim.opt_local.spell = true
 vim.opt_local.conceallevel = 2
+
+-- Only enable spell for regular files, not scratch buffers (LSP hover, etc.)
+if vim.bo.buftype == '' then
+  vim.opt_local.spell = true
+end
 
 vim.treesitter.start()
 
