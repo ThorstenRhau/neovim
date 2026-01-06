a000518e3f27fbe2cb0d80417b3c099916307eea
Thorsten Rhau <thorsten@rhau.se>
2026-01-06 18:44:18 +0100

    fix(ftplugin): restore vim.treesitter.start() for highlighting
    Reverts part of cf9bbcd. The vim.treesitter.start() calls are required
    to enable treesitter highlighting when using nvim-treesitter's main
    branch API, which does not auto-enable highlighting.
    
    Without this, plugins like render-markdown report 'highlighter: not enabled'
    in checkhealth.

--- after/ftplugin/fish.lua
+++ after/ftplugin/fish.lua
@@ -2,6 +2,8 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 
+vim.treesitter.start()
+
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
 vim.wo[0][0].foldmethod = 'expr'
--- after/ftplugin/make.lua
+++ after/ftplugin/make.lua
@@ -1,3 +1,5 @@
 vim.opt_local.expandtab = false
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
+
+vim.treesitter.start()
