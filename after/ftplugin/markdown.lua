5a66ab3b7d4b9d7d94cd860d52d078f9632aac43
Thorsten Rhau <thorsten@rhau.se>
2026-01-06 19:59:59 +0100

    fix(ftplugin): addedd missing indentexpr
    - json
    - markdown
    - toml
    - vim

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -9,5 +9,7 @@ end
 
 vim.treesitter.start()
 
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
 vim.wo[0][0].foldmethod = 'expr'
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
