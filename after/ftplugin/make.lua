6e302faae46ac984f53ce95b574901c07673b887
Thorsten Rhau <thorsten@rhau.se>
2026-01-06 18:56:16 +0100

    fix(treesitter): folding for Makefiles
    Added missing treesitter indentexpr, foldmethod and foldexpr

--- after/ftplugin/make.lua
+++ after/ftplugin/make.lua
@@ -3,3 +3,8 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 
 vim.treesitter.start()
+
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+vim.wo[0][0].foldmethod = 'expr'
+vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
