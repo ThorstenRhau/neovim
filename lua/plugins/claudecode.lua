b663652ece1822a3bd2f971d1f17ece3617994f0
Thorsten Rhau <thorsten@rhau.se>
2026-01-08 15:33:52 +0100

    feat(claude): added plugin for claude code

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,43 @@
+return {
+  'coder/claudecode.nvim',
+
+  keys = {
+    { '<leader>aa', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+    { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+    { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+    { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add buffer to Claude' },
+    { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude session' },
+    { '<leader>aR', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude session' },
+    { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+    { '<leader>ay', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept Claude diff' },
+    { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny Claude diff' },
+  },
+
+  config = function()
+    require('claudecode').setup({
+      terminal = {
+        split_side = 'right',
+        split_width_percentage = 0.40,
+        provider = 'native',
+        show_native_term_exit_tip = true,
+        auto_close = false,
+        env = {},
+        snacks_win_opts = {},
+      },
+    })
+
+    -- Terminal keymaps for claude buffer
+    vim.api.nvim_create_autocmd('TermOpen', {
+      pattern = 'term://*claude*',
+      callback = function()
+        local opts = { buffer = 0 }
+        -- Exit terminal mode
+        vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n><Cmd>wincmd h<CR><Cmd>stopinsert<CR>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      end,
+    })
+  end,
+}
