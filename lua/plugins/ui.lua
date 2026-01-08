1477af82570d433a8608b8f2e35218d8a2f8896d
Thorsten Rhau <thorsten@rhau.se>
2026-01-08 22:36:52 +0100

    feat(which-key): changed to 'modern' preset

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,7 +10,7 @@ return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {
-      preset = 'classic',
+      preset = 'modern',
       delay = 200,
       spec = {
         { '<leader>f', group = 'file/find' },
