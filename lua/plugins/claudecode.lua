500425fd7b2fba550bff1e8528b5d91ac8284182
Thorsten Rhau <thorsten@rhau.se>
2026-01-09 20:12:47 +0100

    refactor(claudecode): convert to opts pattern and add missing keymaps
    - Convert from config = function() to opts = {} pattern per Lazy.nvim best practices
    - Add ClaudeCodeTreeAdd keymap for file tree integration (nvim-tree, neo-tree, oil, minifiles)
    - Add ClaudeCodeStatus keymap for connection status check
    - Remove unused snacks_win_opts when using native provider
    - Rename local opts to keymap_opts to avoid shadowing

--- lua/plugins/claudecode.lua
+++ lua/plugins/claudecode.lua
@@ -9,32 +9,35 @@ return {
     { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude session' },
     { '<leader>aR', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude session' },
     { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+    { '<leader>at', '<cmd>ClaudeCodeTreeAdd<cr>', desc = 'Add tree file to Claude', ft = { 'NvimTree', 'neo-tree', 'oil', 'minifiles' } },
+    { '<leader>aS', '<cmd>ClaudeCodeStatus<cr>', desc = 'Claude status' },
     { '<leader>ay', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept Claude diff' },
     { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny Claude diff' },
   },
 
-  config = function()
-    require('claudecode').setup({
-      terminal = {
-        split_side = 'right',
-        split_width_percentage = 0.40,
-        provider = 'native',
-        auto_close = false,
-        snacks_win_opts = {},
-      },
-    })
+  opts = {
+    terminal = {
+      split_side = 'right',
+      split_width_percentage = 0.40,
+      provider = 'native',
+      auto_close = false,
+    },
+  },
+
+  config = function(_, opts)
+    require('claudecode').setup(opts)
 
     -- Terminal keymaps for claude buffer
     vim.api.nvim_create_autocmd('TermOpen', {
       pattern = 'term://*claude*',
       callback = function()
-        local opts = { buffer = 0 }
+        local keymap_opts = { buffer = 0 }
         -- Exit terminal mode
-        vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n><Cmd>wincmd h<CR><Cmd>stopinsert<CR>]], opts)
-        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n><Cmd>wincmd h<CR><Cmd>stopinsert<CR>]], keymap_opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], keymap_opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], keymap_opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], keymap_opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], keymap_opts)
       end,
     })
   end,
