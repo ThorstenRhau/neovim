12865cef55e5f3435a0fa6d692fdab0a64e8196d
Thorsten Rhau <thorsten@rhau.se>
2026-01-10 11:39:47 +0100

    feat(ts-comments): removed plugin
    - The built in commentstring support via treesitter is used instead

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -80,11 +80,6 @@ return {
     },
   },
   {
-    'folke/ts-comments.nvim',
-    event = 'VeryLazy',
-    opts = {},
-  },
-  {
     'kylechui/nvim-surround',
     event = 'VeryLazy',
     opts = {},
