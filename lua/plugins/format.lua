8acfece963c0808f424e0d923693c77c296e9980
Thorsten Rhau <thorsten@rhau.se>
2026-01-10 13:00:57 +0100

    feat(format): added fallback to =
    When Conform or LSP formatters are not available <leader>cf falls back
    to the neovim built in formatter =

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -10,6 +10,12 @@ return {
           require('conform').format({ async = true, lsp_format = 'fallback' }, function(err)
             if not err then
               vim.notify('File formatted', vim.log.levels.INFO)
+            else
+              -- Fallback to Vim's native = formatting
+              local pos = vim.api.nvim_win_get_cursor(0)
+              vim.cmd('silent! normal! gg=G')
+              vim.api.nvim_win_set_cursor(0, pos)
+              vim.notify('Formatted with native Vim =', vim.log.levels.INFO)
             end
           end)
         end,
@@ -18,7 +24,6 @@ return {
     },
     opts = {
       formatters_by_ft = {
-        ['_'] = { 'trim_whitespace' },
         bash = { 'shfmt' },
         css = { 'prettier' },
         fish = { 'fish_indent' },
