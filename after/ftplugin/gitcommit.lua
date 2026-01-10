d58fb02fe87e692b8854b6b5f2093dfff9d6d1b0
Thorsten Rhau <thorsten@rhau.se>
2026-01-10 11:37:55 +0100

    feat(ftplugins): added missing and removed blanks

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,7 +1,6 @@
-vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
-vim.opt_local.conceallevel = 2
--- Only enable spell for regular files, not scratch buffers (LSP hover, etc.)
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
 if vim.bo.buftype == '' then
   vim.opt_local.spell = true
 end
--- after/ftplugin/vimdoc.lua
+++ after/ftplugin/vimdoc.lua
@@ -1,10 +1,6 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.conceallevel = 2
--- Only enable spell for regular files, not scratch buffers (LSP hover, etc.)
-if vim.bo.buftype == '' then
-  vim.opt_local.spell = true
-end
 vim.treesitter.start()
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 vim.wo[0][0].foldmethod = 'expr'
