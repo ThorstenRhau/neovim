1ee415039bb2b5ef06dbbd4b7710ce62ef89b2d9
Thorsten Rhau <thorsten@rhau.se>
2026-01-16 11:28:36 +0100

    fix(terminal): wrapped in proper lazy config

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,66 +1,71 @@
--- Built-in terminal toggle
--- Simple horizontal terminal at the bottom of the window
+return {
+  dir = vim.fn.stdpath('config'),
+  name = 'terminal',
+  lazy = false,
+  config = function()
+    -- Built-in terminal toggle
+    -- Simple horizontal terminal at the bottom of the window
 
-local term_buf = nil
-local term_win = nil
+    local term_buf = nil
+    local term_win = nil
 
-local function get_height()
-  return math.floor(vim.o.lines * 0.3)
-end
-
-local function open_terminal()
-  -- Create a horizontal split at the bottom
-  vim.cmd('botright split')
-  vim.cmd('resize ' .. get_height())
-
-  if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
-    -- Reuse existing terminal buffer
-    vim.api.nvim_win_set_buf(0, term_buf)
-  else
-    -- Create new terminal
-    vim.cmd('terminal')
-    term_buf = vim.api.nvim_get_current_buf()
-  end
-
-  term_win = vim.api.nvim_get_current_win()
-  vim.cmd('startinsert')
-end
-
-local function close_terminal()
-  if term_win and vim.api.nvim_win_is_valid(term_win) then
-    vim.api.nvim_win_close(term_win, true)
-  end
-  term_win = nil
-end
-
-local function toggle_terminal()
-  if term_win and vim.api.nvim_win_is_valid(term_win) then
-    close_terminal()
-  else
-    open_terminal()
-  end
-end
-
--- Terminal keymaps (applied to all terminals except fzf-lua)
-vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('terminal_keymaps', { clear = true }),
-  callback = function()
-    -- Skip fzf-lua buffers (they handle their own keymaps)
-    local bufname = vim.api.nvim_buf_get_name(0)
-    if bufname:match('fzf') then
-      return
+    local function get_height()
+      return math.floor(vim.o.lines * 0.3)
     end
 
-    local opts = { buffer = 0 }
-    vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    local function open_terminal()
+      -- Create a horizontal split at the bottom
+      vim.cmd('botright split')
+      vim.cmd('resize ' .. get_height())
+
+      if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+        -- Reuse existing terminal buffer
+        vim.api.nvim_win_set_buf(0, term_buf)
+      else
+        -- Create new terminal
+        vim.cmd('terminal')
+        term_buf = vim.api.nvim_get_current_buf()
+      end
+
+      term_win = vim.api.nvim_get_current_win()
+      vim.cmd('startinsert')
+    end
+
+    local function close_terminal()
+      if term_win and vim.api.nvim_win_is_valid(term_win) then
+        vim.api.nvim_win_close(term_win, true)
+      end
+      term_win = nil
+    end
+
+    local function toggle_terminal()
+      if term_win and vim.api.nvim_win_is_valid(term_win) then
+        close_terminal()
+      else
+        open_terminal()
+      end
+    end
+
+    -- Terminal keymaps (applied to all terminals except fzf-lua)
+    vim.api.nvim_create_autocmd('TermOpen', {
+      group = vim.api.nvim_create_augroup('terminal_keymaps', { clear = true }),
+      callback = function()
+        -- Skip fzf-lua buffers (they handle their own keymaps)
+        local bufname = vim.api.nvim_buf_get_name(0)
+        if bufname:match('fzf') then
+          return
+        end
+
+        local opts = { buffer = 0 }
+        vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      end,
+    })
+
+    vim.keymap.set('n', '<leader>tt', toggle_terminal, { desc = 'terminal' })
   end,
-})
+}
 
-vim.keymap.set('n', '<leader>tt', toggle_terminal, { desc = 'terminal' })
-
--- Return empty table (no plugin to load)
-return {}
