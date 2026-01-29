0d6c774d035938a596355f0e3b26be5356ed87e6
Thorsten Rhau <thorsten@rhau.se>
2026-01-29 21:58:16 +0100

    fix(treesitter): change textobjects load event to BufReadPost

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -54,7 +54,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
     branch = 'main',
-    event = 'VeryLazy',
+    event = 'BufReadPost',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
       require('nvim-treesitter-textobjects').setup({
