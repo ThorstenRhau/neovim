469f4e13cbd8d82b41440d212e4d052f7645efd3
Thorsten Rhau <thorsten@rhau.se>
2026-01-29 16:08:11 +0100

    fix(vim-matchup): now loading on BufReadPre
    The previously used VeryLazy did not work. Also added some more
    detailed treesitter configuration.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -27,9 +27,14 @@ return {
   },
   {
     'andymass/vim-matchup',
-    event = 'VeryLazy',
+    event = 'BufReadPre',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     init = function()
       vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+      vim.g.matchup_matchparen_deferred = 1
+      vim.g.matchup_treesitter_stopline = 500
+      vim.g.matchup_treesitter_include_match_words = false
+      vim.g.matchup_treesitter_enable_quotes = true
     end,
   },
   {
