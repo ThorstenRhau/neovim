66dcad81af30fd68c0b69ed91d46896519afaa73
Thorsten Rhau <thorsten@rhau.se>
2026-04-01 23:21:43 +0200

    feat(options): enable experimental Neovim UI2 with floating cmdline and messages

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -82,3 +82,52 @@ o.spellsuggest = 'best,20' -- Limits to 20 suggestions
 -- Colorscheme (colors/claude-theme.lua is in rtp from stdpath('config'))
 o.termguicolors = true
 vim.cmd.colorscheme('claude-theme')
+
+-- Experimental UI2: floating cmdline and messages
+o.cmdheight = 0
+require('vim._core.ui2').enable({
+  enable = true,
+  msg = {
+    targets = {
+      [''] = 'cmd',
+      empty = 'cmd',
+      bufwrite = 'msg',
+      confirm = 'cmd',
+      emsg = 'msg',
+      echo = 'msg',
+      echomsg = 'msg',
+      echoerr = 'msg',
+      completion = 'cmd',
+      list_cmd = 'pager',
+      lua_error = 'msg',
+      lua_print = 'msg',
+      progress = 'msg',
+      rpc_error = 'msg',
+      quickfix = 'msg',
+      search_cmd = 'cmd',
+      search_count = 'cmd',
+      shell_cmd = 'pager',
+      shell_err = 'pager',
+      shell_out = 'pager',
+      shell_ret = 'msg',
+      undo = 'msg',
+      verbose = 'pager',
+      wildlist = 'cmd',
+      wmsg = 'msg',
+      typed_cmd = 'cmd',
+    },
+    cmd = {
+      height = 0.5,
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.5,
+      timeout = 4000,
+    },
+    pager = {
+      height = 3,
+    },
+  },
+})
