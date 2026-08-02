e98d4d335f6745be02dab076cd59199f6b755f70
Thorsten Rhau <thorsten@rhau.se>
2026-08-02 15:16:37 +0200

    fix(gitcommit): restore Neogit commit guides

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,3 +1,10 @@
 vim.opt_local.textwidth = 72
 vim.opt_local.colorcolumn = '50,73'
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  buffer = 0,
+  desc = 'Show commit message guides in newly opened windows',
+  callback = function()
+    vim.opt_local.colorcolumn = '50,73'
+  end,
+})
 require('config.ftplugin').prose().indent(2).treesitter()
