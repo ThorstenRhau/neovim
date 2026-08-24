5c80cfe3d0f93d7e5e750ad0a434ce52abe8b7be
Thorsten Rhau <thorsten@rhau.se>
2026-08-24 16:09:21 +0200

    feat(keymaps): use native Neovim restart

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -138,7 +138,7 @@ map('n', '<leader>l', function()
 end, { desc = 'update plugins' })
 
 -- Session restart
-map('n', '<leader>R', '<cmd>lua MiniSessions.restart()<cr>', { desc = 'restart Neovim' })
+map('n', '<leader>R', '<cmd>restart<cr>', { desc = 'restart Neovim' })
 
 -- Toggle options
 local toggles = {
