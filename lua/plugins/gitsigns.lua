779217089ac4d3c63f9b827164c542a5a239219e
Thorsten Rhau <thorsten@rhau.se>
2026-08-31 23:36:33 +0200

    feat(gitsigns): toggle revision diffs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,30 @@
 local map = vim.keymap.set
 
+local function close_revision_diffs()
+  local revision_wins = {}
+
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.api.nvim_buf_get_name(buf):match('^gitsigns://') then
+      table.insert(revision_wins, win)
+    end
+  end
+
+  for _, win in ipairs(revision_wins) do
+    vim.api.nvim_win_close(win, false)
+  end
+
+  return #revision_wins > 0
+end
+
+local function toggle_diffthis(base)
+  if vim.wo.diff and close_revision_diffs() then
+    return
+  end
+
+  require('gitsigns').diffthis(base)
+end
+
 require('gitsigns').setup({
   attach_to_untracked = false,
   signs = {
@@ -56,6 +81,17 @@ require('gitsigns').setup({
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  group = vim.api.nvim_create_augroup('gitsigns_revision_exit', { clear = true }),
+  callback = function(event)
+    if not vim.api.nvim_buf_get_name(event.buf):match('^gitsigns://') then
+      return
+    end
+
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buf = event.buf, desc = 'close revision diff' })
+  end,
+})
+
 map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<cr>', { desc = 'stage hunk' })
 map('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<cr>', { desc = 'reset hunk' })
 map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<cr>', { desc = 'stage buffer' })
@@ -63,8 +99,9 @@ map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<cr>', { desc = 'reset buffer'
 map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk_inline<cr>', { desc = 'preview hunk' })
 map('n', '<leader>hb', '<cmd>Gitsigns blame_line full=true<cr>', { desc = 'blame line' })
 map('n', '<leader>hB', '<cmd>Gitsigns blame<cr>', { desc = 'blame buffer' })
-map('n', '<leader>gd', '<cmd>Gitsigns diffthis<cr>', { desc = 'diff this' })
-map('n', '<leader>hd', '<cmd>Gitsigns diffthis<cr>', { desc = 'diff this' })
-map('n', '<leader>hD', '<cmd>Gitsigns diffthis ~<cr>', { desc = 'diff this ~' })
+map('n', '<leader>gd', toggle_diffthis, { desc = 'toggle diff this' })
+map('n', '<leader>hD', function()
+  toggle_diffthis('~')
+end, { desc = 'toggle diff this ~' })
 map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<cr>', { desc = 'line blame' })
 map('n', '<leader>hl', '<cmd>Gitsigns toggle_linehl<cr>', { desc = 'toggle line highlight' })
