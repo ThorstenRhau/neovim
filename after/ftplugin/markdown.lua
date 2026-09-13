74f9e4688d70c402f82375180ae1df3055f88c55
Thorsten Rhau <thorsten@rhau.se>
2026-09-13 11:47:55 +0200

    feat(spelling): disabled spelling in markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,3 @@
 vim.opt_local.wrap = true
-vim.opt_local.spell = true
 vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
-  .. '|call map(win_findbuf(bufnr()), {_, win -> win_execute(win, "setlocal wrap< spell<")})'
+  .. '|call map(win_findbuf(bufnr()), {_, win -> win_execute(win, "setlocal wrap<")})'
